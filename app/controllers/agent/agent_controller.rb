495e9b3fef31164bfa822d4b5fead80e5f686ebc
tgn <rb@rb-desktop.(none)>
2010-10-20 16:35:21 +0200

    security added to agent controller

--- app/controllers/agent/agent_controller.rb
+++ app/controllers/agent/agent_controller.rb
@@ -1,2 +1,12 @@
 class Agent::AgentController < SecuredController
+  def current_user
+    #Second request is cached so only class-casting occurs
+    @agent ||= ::User::Agent.find(super.id)
+  end
+
+  private
+
+  def authorize_user_for_namespace!
+    authorize_role(:agent)
+  end
 end
