a3ca1f9138d40746b53fcc089c360de21165d888
tgn <rb@rb-desktop.(none)>
2010-10-26 09:42:13 +0200

    #2421 add translations to certain lead fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include ScopedSearch::Helpers
+
+  def link_to_add_fields(f)
+     new_object = f.object.class.new
+     fields = fields_for(new_object) do |builder|
+       render("lead_fields", :f => builder)
+     end
+     "add_fields(this, \"#{escape_javascript(fields)}\")"
+  end
 end
