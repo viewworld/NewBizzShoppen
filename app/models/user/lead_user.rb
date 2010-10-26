d1aa8ca1e70c41638582a7e819cda5a43ff9b0bc
Blazej Kosmowski <bk@bk-selleo.(none)>
2010-10-26 16:04:34 +0200

    LeadRequests basics

--- app/models/user/lead_user.rb
+++ app/models/user/lead_user.rb
@@ -1,3 +1,12 @@
 class ::User::LeadUser < ::User
   ROLES = [:lead_user]
+
+  has_many :lead_requests, :foreign_key => :requested_by, :after_add => :set_owner
+
+  private
+
+  def set_owner(lead_request)
+    lead_request.update_attribute(:owner_id, parent_id || id)
+  end
+
 end
--- spec/models/lead_request_spec.rb
+++ spec/models/lead_request_spec.rb
@@ -12,6 +12,26 @@ describe Lead do
     }.should change(LeadRequest, :count).by(3)
   end
 
+  context "Creation and scoping" do
+
+    it "should properly assign itself to creator" do
+      lead_user = User::LeadUser.make!
+      lead = Lead.make!
+      lead_user.lead_requests.create!(:lead_id => lead.id)
+
+      lead_user.lead_requests.should have(1).things
+    end
+
+    it "should propely be visible by owner" do
+     customer = User::Customer.make!
+      lead_user = User::LeadUser.make!(:parent_id => customer.id)
+      lead = Lead.make!
+      lead_request = lead_user.lead_requests.create!(:lead_id => lead.id)
+      customer.lead_requests.should include(lead_request)
+    end
+
+
+  end
 
 
 end
