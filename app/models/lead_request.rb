087a45b394e7e40fecb4c375511f2d19ceaa27a7
Blazej Kosmowski <bk@bk-selleo.(none)>
2010-10-27 10:09:19 +0200

    LeadRequest -> accepting/rejecting

--- app/models/lead_request.rb
+++ app/models/lead_request.rb
@@ -1,4 +1,17 @@
 class LeadRequest < ActiveRecord::Base
   set_table_name "lead_purchases"
   default_scope where("requested_by IS NOT NULL")
+
+  belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
+
+  def accept!
+    self.assignee_id = requested_by
+    self.requested_by = nil
+    self.accessible = true if owner.big_buyer?
+    self.save
+  end
+
+  def reject!
+    self.destroy
+  end
 end
--- spec/models/lead_request_spec.rb
+++ spec/models/lead_request_spec.rb
@@ -13,7 +13,6 @@ describe Lead do
   end
 
   context "Creation and scoping" do
-
     it "should properly assign itself to creator" do
       lead_user = User::LeadUser.make!
       lead = Lead.make!
@@ -22,15 +21,60 @@ describe Lead do
       lead_user.lead_requests.should have(1).things
     end
 
-    it "should propely be visible by owner" do
-     customer = User::Customer.make!
+    it "should properly be visible by owner" do
+      customer = User::Customer.make!
       lead_user = User::LeadUser.make!(:parent_id => customer.id)
       lead = Lead.make!
       lead_request = lead_user.lead_requests.create!(:lead_id => lead.id)
       customer.lead_requests.should include(lead_request)
     end
+  end
 
+  context "Processing" do
+    it "should be accepted properly for regular buyer" do
+      customer = User::Customer.make!
+      lead_user = User::LeadUser.make!(:parent_id => customer.id)
+      lead = Lead.make!
+      lead_request = lead_user.lead_requests.create!(:lead_id => lead.id)
 
+      lambda {
+        lambda {
+          lead_request.accept!
+        }.should change(LeadRequest, :count).by(-1)
+      }.should change(LeadPurchase, :count).by(1)
+
+      (lead_purchase = LeadPurchase.find_by_id(lead_request.id)).should_not be nil
+      lead_purchase.accessible?.should be false
+      lead_purchase.assignee_id.should be lead_user.id
+    end
+
+    it "should be accepted properly by big buyer" do
+      customer = User::Customer.make!(:big_buyer => true)
+      lead_user = User::LeadUser.make!(:parent_id => customer.id)
+      lead = Lead.make!
+      lead_request = lead_user.lead_requests.create!(:lead_id => lead.id)
+
+      lead_request.accept!
+
+      (lead_purchase =  LeadPurchase.find_by_id(lead_request.id)).should_not be nil
+      lead_purchase.accessible?.should be true
+    end
+
+    it "should be rejected properly" do
+      customer = User::Customer.make!
+      lead_user = User::LeadUser.make!(:parent_id => customer.id)
+      lead = Lead.make!
+      lead_request = lead_user.lead_requests.create!(:lead_id => lead.id)
+
+      lambda {
+        lambda {
+          lead_request.reject!
+        }.should change(LeadRequest, :count).by(-1)
+      }.should_not change(LeadPurchase, :count)
+
+      LeadPurchase.find_by_id(lead_request.id).should be nil
+      LeadRequest.find_by_id(lead_request.id).should be nil
+    end
   end
 
 
