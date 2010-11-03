1291374357874053c3939402afa51c2621e477d5
tgn <rb@rb-desktop.(none)>
2010-11-03 09:10:43 +0100

    #2307 basic users administration - edit (tests)

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -16,3 +16,9 @@ end
 Given /^Country (.+) is created$/ do |name|
   Country.create(:name => name)
 end
+
+Given /^I run ruby "([^\"]*)"$/ do |ruby|
+print "result of: #{ruby}\n\n"
+eval ruby
+print "\n\n"
+end
