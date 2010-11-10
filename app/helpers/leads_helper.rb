470f5ccae6439d629495b0c9c183754e84d6eb52
tgn <rb@rb-desktop.(none)>
2010-11-10 09:57:33 +0100

    Merge branch 'master' of github.com:Selleo/NewBizzShoppen
    Conflicts:
    	app/models/lead_purchase.rb

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -5,7 +5,11 @@ module LeadsHelper
     options[:show_checkboxes] ||= false
     render(:partial => '/leads/table', :locals => options.merge({:collection => collection}.merge(@hb.results)))
   end
+
+  def leads_listing(collection, options = {}, &block)
+    @hb = ApplicationHelper::HelperBlocks.new(:left, :right)
+    block.call(@hb)
+    options[:show_checkboxes] ||= false
+    render(:partial => '/leads/listing', :locals => options.merge({:collection => collection}.merge(@hb.results)))
+  end
 end
-
-
-
