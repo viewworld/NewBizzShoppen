538436494b401150209e3f399eb63367ec759d98
tgn <rb@rb-desktop.(none)>
2010-11-25 09:39:52 +0100

    #2634 handling routing errors - bulk acion redirect_to back problem

--- lib/bulk_process.rb
+++ lib/bulk_process.rb
@@ -7,7 +7,6 @@ class BulkProcess
   def call(env)
     req = Rack::Request.new(env)
     req.params["_method"] = "POST"
-
     if env['rack.request.form_hash'] && env['rack.request.form_hash']['route_to']
       new_url, new_method = env['rack.request.form_hash']['route_to'].split("#")
       env['PATH_INFO'] =   new_url
@@ -17,12 +16,11 @@ class BulkProcess
         env["REQUEST_METHOD"] = new_method
         req.params["_method"] = new_method
       end
+    session = env['rack.session']
+    session[:bulk_action_back] = env["REQUEST_METHOD"] == "GET" ? new_url : env["HTTP_REFERER"]
     end
     status, headers, response = @app.call(env)
 
     [status, headers, response]
   end
-
-
-
 end
