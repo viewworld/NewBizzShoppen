9fdcaa92e424d1c251ed02c6fd8f517575e31e5f
tgn <rb@rb-desktop.(none)>
2010-12-14 11:22:22 +0100

    #2982 leads catalog for big buyer

--- app/controllers/buyers/bulk_cart_items_controller.rb
+++ app/controllers/buyers/bulk_cart_items_controller.rb
@@ -2,7 +2,11 @@ class Buyers::BulkCartItemsController < Buyers::BuyerController
 
   def create
     flash[:notice] = if params[:lead_ids]
+                      if current_user.big_buyer?
+                        t("buyer.bulk_cart_items.create.flash.n_leads_bought", :count => (current_user.cart.add_leads(params[:lead_ids])).size)
+                      else
                        t("buyer.bulk_cart_items.create.flash.n_cart_items_added", :count => (current_user.cart.add_leads(params[:lead_ids])).size)
+                      end
                      else
                        t("buyer.bulk_cart_items.create.flash.no_cart_items_added")
                      end
