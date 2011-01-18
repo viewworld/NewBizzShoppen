670c2b152e7d397018e613d0d265a8d9942065f4
tgn <rb@rb-desktop.(none)>
2011-01-18 16:09:48 +0100

    #3380 call centre payout information update

--- app/models/user/call_centre.rb
+++ app/models/user/call_centre.rb
@@ -2,4 +2,10 @@ class ::User::CallCentre < ::User
   ROLES = [:call_centre]
 
   validates_inclusion_of :payout, :in => 0..100, :message =>   I18n.t("models.user.payout_validation_message")
+
+  before_create :allow_payout_information_editing
+
+  def allow_payout_information_editing
+    self.can_edit_payout_information = true
+  end
 end
