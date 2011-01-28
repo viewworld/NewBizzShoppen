a0b3cbd87244e7bbac3c27e7c0232ad97af60afe
Adrian Ossowski <aossowski@gmail.com>
2011-01-28 16:57:20 +0100

    paypal

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -6,6 +6,7 @@ class PaymentNotificationsController < ApplicationController
   def create
     params[:invoice].to_s.slice!(0..7)
     payment_notification = PaymentNotification.create!(:params => params, :buyer_id => params[:invoice].to_i, :status => params[:payment_status], :transaction_id => params[:txn_id])
+    puts "#{payment_notification.status} == \"Completed\" &&  #{params[:secret]} == #{APP_CONFIG[:paypal_secret]} && #{params[:receiver_email]} == #{APP_CONFIG[:paypal_email]} && #{params[:mc_gross]} == #{ActionView::Helpers::NumberHelper.number_with_precision(payment_notification.buyer.cart.total, :precision => 2)}"
     if payment_notification.status == "Completed" &&
         params[:secret] == APP_CONFIG[:paypal_secret] &&
         params[:receiver_email] == APP_CONFIG[:paypal_email] &&
