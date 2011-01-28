5d2b8936bd03ae7015292815c7ad11296c8a1046
Adrian Ossowski <aossowski@gmail.com>
2011-01-28 17:04:57 +0100

    paypal

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,16 +1,14 @@
 class PaymentNotificationsController < ApplicationController
-  include ActionView::Helpers::NumberHelper
   protect_from_forgery :except => [:create]
   before_filter :authorize_with_http_basic_for_staging, :except => [:create]
 
   def create
     params[:invoice].to_s.slice!(0..7)
     payment_notification = PaymentNotification.create!(:params => params, :buyer_id => params[:invoice].to_i, :status => params[:payment_status], :transaction_id => params[:txn_id])
-    puts "#{payment_notification.status} == \"Completed\" &&  #{params[:secret]} == #{APP_CONFIG[:paypal_secret]} && #{params[:receiver_email]} == #{APP_CONFIG[:paypal_email]} && #{params[:mc_gross]} == #{ActionView::Helpers::NumberHelper.number_with_precision(payment_notification.buyer.cart.total, :precision => 2)}"
     if payment_notification.status == "Completed" &&
         params[:secret] == APP_CONFIG[:paypal_secret] &&
         params[:receiver_email] == APP_CONFIG[:paypal_email] &&
-        params[:mc_gross] == ActionView::Helpers::NumberHelper.number_with_precision(payment_notification.buyer.cart.total, :precision => 2)
+        params[:mc_gross] == payment_notification.buyer.cart.total.to_s
       payment_notification.buyer.cart.paid!
     end
     render :nothing => true
