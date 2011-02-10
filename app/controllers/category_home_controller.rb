4df10b75161bab38aa53ee828d078417c3b9c9f7
Adrian Ossowski <aossowski@gmail.com>
2011-02-10 10:13:19 +0100

    #3663

--- app/controllers/category_home_controller.rb
+++ app/controllers/category_home_controller.rb
@@ -5,6 +5,7 @@ class CategoryHomeController < ApplicationController
   def show
     @latest_leads = Lead.published_only.with_category(@home_category).without_bought_and_requested_by(current_user).latest.limit(10)
     @news         = @home_category.news.published.latest.limit(3)
+    @my_leads     = current_user.bought_leads.latest.limit(3)
   end
 
 end
