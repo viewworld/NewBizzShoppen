22f762cbbf7e417ca835da03afd36f6a4566b571
rafal <rafal.bromirski@gmail.com>
2011-02-19 18:58:23 +0100

    invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,14 +1,12 @@
 class Invoice < ActiveRecord::Base
   PAYMENT_METHODS = [:bank_transfer, :cash]
 
-  MARKUP_SCAFFOLD = %{<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-        <html>
+  MARKUP_SCAFFOLD = %{<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
           <head>
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
             <style type="text/css">
-              @import url("unicode.css") print;
               @import url("../stylesheets/invoice.css") print;
-              @import url("css.css") print;
             </style>
           </head>
           <body>
