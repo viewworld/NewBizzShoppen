7c2fdf74896452e466bd68904d621af5949e8d42
Adrian Ossowski <aossowski@gmail.com>
2011-02-21 13:45:36 +0100

    #3805

--- app/helpers/lead_templates_helper.rb
+++ app/helpers/lead_templates_helper.rb
@@ -7,4 +7,8 @@ module LeadTemplatesHelper
     options[:show_active_column] ||= false
     render(:partial => '/shared/lead_templates/table', :locals => options.merge({:collection => collection}.merge(@hb.results)))
   end
+
+  def translate_boolean(val)
+    val ? t("lead_templates.table.yes_label") : t("lead_templates.table.no_label")
+  end
 end
