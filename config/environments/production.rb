b1bbdd51e7908f03244e45b0bb78597ca3241f3f
Adrian Ossowski <aossowski@gmail.com>
2011-02-22 17:09:50 +0100

    disable cucumber fm in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Nbs::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  config.plugins = [ :e_template, :stringex, :template_derby ]
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes                     = true
