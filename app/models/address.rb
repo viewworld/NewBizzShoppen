e58ea372838db01de0558b168b7b46a867beecea
tgn <rb@rb-desktop.(none)>
2011-03-01 15:32:05 +0100

    #3974 call centre corrections

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,9 @@ class Address < ActiveRecord::Base
   belongs_to :addressable, :polymorphic => true
   belongs_to :country
 
-  validates_presence_of :address_line_1, :zip_code, :country
+  validates_presence_of :address_line_1, :zip_code, :country#, :unless => Proc.new { |a| ActiveRecord::ConnectionAdapters::Column.value_to_boolean(a.disabled_validations) == true  }
+
+  #attr_accessor :disabled_validations
 
   def to_s
     "#{address_line_1}, #{zip_code} #{address_line_2}, #{address_line_3}"
