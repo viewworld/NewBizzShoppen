6ab0d635346da3ade27e4960b07b544e592b107a
tgn <rb@rb-desktop.(none)>
2011-03-01 13:45:44 +0100

    #3974 call centre corrections

--- app/models/user/call_centre.rb
+++ app/models/user/call_centre.rb
@@ -4,9 +4,15 @@ class ::User::CallCentre < ::User
   include Addresses
   include BankAccounts
 
+  has_one :bank_address, :class_name => '::Address::Bank', :as => :addressable
+
   validates_inclusion_of :payout, :in => 0..100, :message =>   I18n.t("models.user.payout_validation_message")
 
   before_create :allow_payout_information_editing
+
+  after_initialize :build_bank_address_object
+
+  accepts_nested_attributes_for :bank_address
 
   def allow_payout_information_editing
     self.can_edit_payout_information = true
@@ -14,5 +20,13 @@ class ::User::CallCentre < ::User
 
   def can_publish_leads?
     true
+  end
+
+  private
+
+  def build_bank_address_object
+    if new_record? or !bank_address
+      build_bank_address
+    end
   end
 end
