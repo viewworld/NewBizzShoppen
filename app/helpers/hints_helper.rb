a94ea056f3b190c16f1488e18b66def375d50bbf
rafal <rafal.bromirski@gmail.com>
2011-03-15 12:09:31 +0100

    nbs_m6_gui / tip for hints

--- app/helpers/hints_helper.rb
+++ app/helpers/hints_helper.rb
@@ -3,10 +3,9 @@ module HintsHelper
     html_id = "#{object.class.to_s.tableize.singularize}_#{method}"
     @hint = Article::Cms::Hint.first(:conditions => {:key => html_id, :published => true})
     if !@hint.blank? and !@hint.content.blank?
-      return link_to_function("[?]", "javascript:void();", {:id => "hint_#{html_id}"}) +
-          "<script>$('#hint_#{html_id}').simpletip({ content: '#{@hint.content_sanitized}', fixed: true });</script>".html_safe
+      return link_to_function(image_tag("icons/hint.png"), "javascript:void();", {:id => "hint_#{html_id}", :class => "inputs_hint"}) +
+              content_tag(:div, @hint.content_sanitized.html_safe, :id => "hint_#{html_id}", :class => "hidden")
     end
-
     nil
   end
 end
