8847c3ed5cf04de6a63775e3910a2917be4e31a4
rafal <rafal.bromirski@gmail.com>
2011-03-23 16:14:01 +0100

    m6 and m7 gui

--- app/helpers/hints_helper.rb
+++ app/helpers/hints_helper.rb
@@ -2,7 +2,7 @@ module HintsHelper
   def hint_for(object, method)
     html_id = "#{object.class.to_s.tableize.singularize}_#{method}"
     @hint = Article::Cms::Hint.first(:conditions => {:key => html_id, :published => true})
-    admin_edit_link = (current_user and current_user.has_role?(:admin)) ? link_to(image_tag("icons/edit.png"), edit_administration_hint_path(@hint), :title => t("common.edit_hint")) : ""
+    admin_edit_link = (current_user and current_user.has_role?(:admin)) ? content_tag(:p, link_to(image_tag("icons/edit.png"), edit_administration_hint_path(@hint), :title => t("common.edit_hint")), :class => "inline-hints") : ""
     if !@hint.blank? and !@hint.content.blank?
       return link_to_function(image_tag("icons/hint.png"), "javascript:void();", {:id => "hint_#{html_id}", :class => "inputs_hint"}) +
               content_tag(:div, (@hint.content_sanitized).html_safe, :id => "hint_#{html_id}", :class => "hidden") +
