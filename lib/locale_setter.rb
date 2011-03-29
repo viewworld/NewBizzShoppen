2260da91e4e05b0287ef90b7d20dc9ad09d71d94
Adrian Ossowski <aossowski@gmail.com>
2011-03-29 14:57:23 +0200

    #4649

--- lib/locale_setter.rb
+++ lib/locale_setter.rb
@@ -6,8 +6,9 @@ module Rack
 
     def call env
       dictionary = {'da' => 'dk'}
-      locale = env['rack.session'][:locale_code] || env["HTTP_ACCEPT_LANGUAGE"].to_s[/^([a-z]{2})/] || 'en'
-      env['rack.session'][:locale_code] = (dictionary[locale.to_s] || locale.to_s)
+      locale = env['rack.session'][:locale_code] || env["HTTP_ACCEPT_LANGUAGE"].to_s[/^([a-z]{2})/]
+      locale = (dictionary[locale.to_s] || locale.to_s)
+      env['rack.session'][:locale_code] = Locale.all.map(&:code).include?(locale) ? locale : 'en'
       I18n.locale = env['rack.session'][:locale_code]
       @app.call env
     end
