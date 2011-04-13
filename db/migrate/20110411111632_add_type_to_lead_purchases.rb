05b600fa205984ba0b154d1e9f53a4b271fe3801
Adrian Ossowski <aossowski@gmail.com>
2011-04-13 11:27:48 +0200

    buyout

--- db/migrate/20110411111632_add_type_to_lead_purchases.rb
+++ db/migrate/20110411111632_add_type_to_lead_purchases.rb
@@ -1,8 +1,9 @@
 class AddTypeToLeadPurchases < ActiveRecord::Migration
   def self.up
     add_column :lead_purchases, :type, :string
-#    execute "UPDATE lead_requests SET type = 'LeadRequest' WHERE requested_by IS NOT NULL"
-#    execute "UPDATE lead_requests SET type = ''"
+    execute "UPDATE lead_purchases SET type = 'LeadRequest' WHERE requested_by IS NOT NULL"
+    execute "UPDATE lead_purchases SET type = 'LeadSinglePurchase' WHERE requested_by IS NULL and quantity = 1"
+    execute "UPDATE lead_purchases SET type = 'LeadBuyout' WHERE requested_by IS NULL and quantity <> 1"
   end
 
   def self.down
