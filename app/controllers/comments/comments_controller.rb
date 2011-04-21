011cf29d38205a43c0ec1e11f7dc58825f78ddc7
tgn <rb@rb-desktop.(none)>
2011-04-21 12:27:02 +0200

    #5095 comments

--- app/controllers/comments/comments_controller.rb
+++ app/controllers/comments/comments_controller.rb
@@ -2,7 +2,7 @@ class Comments::CommentsController < SecuredController
   private
 
   def authorize_user_for_namespace!
-    raise CanCan::AccessDenied unless user_signed_in?
+    raise CanCan::AccessDenied if !user_signed_in? or (current_user and current_user.has_role?(:purchase_manager))
   end
 
   def current_user
