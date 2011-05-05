5fa8893c018e514d8cb9a3425837a0b72cd813b4
Adrian Ossowski <aossowski@gmail.com>
2011-05-05 11:57:58 +0200

    translations cache

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -2,7 +2,11 @@
 require 'i18n/active_record'
 I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
 I18n.backend = I18n::Backend::ActiveRecord.new
-I18n.cache_store = ActiveSupport::Cache.lookup_store(:mem_cache_store)
+if ['staging','production'].include?(Rails.env)
+  I18n.cache_store = ActiveSupport::Cache.lookup_store(:mem_cache_store)
+else
+  I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+end
 
 # Fallbacks
 require "i18n/backend/fallbacks"
