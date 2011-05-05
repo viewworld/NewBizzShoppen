e87f60d5893c69594ea42beb679b94fa98ff4200
Adrian Ossowski <aossowski@gmail.com>
2011-05-05 12:53:37 +0200

    paypal

--- app/modules/paypal_payment.rb
+++ app/modules/paypal_payment.rb
@@ -1,8 +1,8 @@
 module PaypalPayment
 
-  PAYPAL_CERT_PEM = File.read("#{Rails.root}/certs/paypal_cert.pem")
-  APP_CERT_PEM    = File.read("#{Rails.root}/certs/app_cert.pem")
-  APP_KEY_PEM     = File.read("#{Rails.root}/certs/app_key.pem")
+  PAYPAL_CERT_PEM = File.read("#{Rails.root}/certs/#{Rails.env == 'production' ? 'production' : 'staging'}/paypal_cert.pem")
+  APP_CERT_PEM    = File.read("#{Rails.root}/certs/#{Rails.env == 'production' ? 'production' : 'staging'}/app_cert.pem")
+  APP_KEY_PEM     = File.read("#{Rails.root}/certs/#{Rails.env == 'production' ? 'production' : 'staging'}/app_key.pem")
 
   def encrypt_for_paypal(values)
     signed = OpenSSL::PKCS7::sign(OpenSSL::X509::Certificate.new(APP_CERT_PEM), OpenSSL::PKey::RSA.new(APP_KEY_PEM, ''), values.map { |k, v| "#{k}=#{v}" }.join("\n"), [], OpenSSL::PKCS7::BINARY)
