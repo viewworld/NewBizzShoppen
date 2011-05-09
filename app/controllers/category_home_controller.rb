08e62685ca0500adaabcbba8c8218934140a0deb
tgn <rb@rb-desktop.(none)>
2011-05-09 09:54:20 +0200

    my leads on the category home fixed

--- app/controllers/category_home_controller.rb
+++ app/controllers/category_home_controller.rb
@@ -5,7 +5,7 @@ class CategoryHomeController < ApplicationController
   def show
     @latest_leads = Lead.without_inactive.published_only.with_category(@home_category).without_bought_and_requested_by(current_user).latest.limit(3)
     @news         = @home_category.news.published.latest.limit(3)
-    @my_leads     = current_user.bought_leads.latest.limit(3) if user_signed_in? and current_user.has_role? :category_buyer
+    @my_leads     = (user_signed_in? and current_user.has_role? :category_buyer) ? current_user.bought_leads.latest.limit(3) : []
   end
 
 end
