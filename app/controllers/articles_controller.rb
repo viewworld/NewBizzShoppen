7ba0f25481fb069631d5562d1a2423be7d97b9d8
tgn <rb@rb-desktop.(none)>
2011-05-09 09:14:18 +0200

    articles - DoubleRenderError fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   inherit_resources
 
+  def index
+    redirect_to root_path
+  end
+
   def show
     unless @article = Article.published.where(:id => params[:id]).first
       redirect_to root_path
