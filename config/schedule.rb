03913a5ab0d5058eccf486fc31390bef6bbfaaee
tgn <rb@rb-desktop.(none)>
2011-05-17 11:02:56 +0200

    #5497 lead certification - crontab settings

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,8 +5,8 @@
 
 set :output, File.join(Dir.pwd, "log/cron_log.log")
 
-hours = %w{1 1 2 2 3 3}
-['recalculate_leads_average_ratings', 'refresh_subaccounts_counters', 'refresh_agent_counters', 'refresh_buyer_counters'].each_with_index do |thor_task, i|
+hours = %w{1 1 2 2 3 3 5}
+['recalculate_leads_average_ratings', 'refresh_subaccounts_counters', 'refresh_agent_counters', 'refresh_buyer_counters', 'check_lead_certification_requests'].each_with_index do |thor_task, i|
   every 1.day, :at => "#{hours[i]}:#{i % 2 == 0 ? '00' : '30' } am" do
     command File.join(Dir.pwd, "RAILS_ENV=production thor nbs:#{thor_task}")
   end
