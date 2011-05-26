85225499c429978ac1008bb89b96411276a1de64
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-05-26 10:15:54 +0200

    ajax upload

--- app/controllers/callers/materials_controller.rb
+++ app/controllers/callers/materials_controller.rb
@@ -1,7 +1,7 @@
 class Callers::MaterialsController < Callers::CallerController
   inherit_resources
   set_tab "campaigns"
-  layout 'campaign_materials'
+  layout proc{ |c| c.request.xhr? ? "send_material" : "application" }
 
   before_filter :fetch_campaign
   before_filter lambda {authorize_access_rights(@campaign)}
@@ -15,12 +15,28 @@ class Callers::MaterialsController < Callers::CallerController
   public
 
   def index
-    @materials = @campaign.materials
-    @material = @campaign.materials.new(params[:material])
+    @materials = @campaign.materials.all
+    @material = @campaign.materials.build
+    respond_to do |wants|
+      wants.html
+      wants.js {
+        render :partial => 'materials', :locals => {:materials => @materials, :material => @material}
+      }
+    end
   end
 
   def create
+    @materials = @campaign.materials.all
     @material = @campaign.materials.build(params[:material])
+    respond_to do |wants|
+      if @material.save
+        wants.html { redirect_to :action => :index }
+        wants.js
+      else
+        wants.html { render :action => :index }
+        wants.js
+      end
+    end
   end
 
   def destroy
