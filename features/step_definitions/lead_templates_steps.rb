56476a07b0c1b5759acd25131548bee230124e6b
tgn <rb@rb-desktop.(none)>
2011-05-30 11:18:17 +0200

    #5780 test corrections -- corrected steps after gems update

--- features/step_definitions/lead_templates_steps.rb
+++ features/step_definitions/lead_templates_steps.rb
@@ -9,17 +9,17 @@ Given /^template named "([^"]*)" for category "([^"]*)" is created by user "([^"
 end
 
 Given /^template named "([^"]*)" (is|is not) mandatory$/ do |name, is_mandatory|
-  template = LeadTemplate.find_by_name(name)
+  template = LeadTemplate.where(:name => name).first
   template.update_attributes(:is_mandatory => is_mandatory != "is not" )
 end
 
 Given /^template named "([^"]*)" (is|is not) active$/ do |name, is_active|
-  template = LeadTemplate.find_by_name(name)
+  template = LeadTemplate.where(:name => name).first
   template.update_attributes(:is_active => is_active != "is not" )
 end
 
 Given /^template named "([^"]*)" (is|is not) global$/ do |name, is_global|
-  template = LeadTemplate.find_by_name(name)
+  template = LeadTemplate.where(:name => name).first
   template.update_attributes(:is_global => is_global != "is not" )
 end
 
@@ -36,7 +36,7 @@ end
 #values are entered as pairs field name:
 Given /^template named "([^"]*)" for lead "([^"]*)" has values "([^"]*)"$/ do |template_name, lead_header, values|
   lead = Lead.where(:header => lead_header).first
-  template = LeadTemplate.find_by_name(template_name)
+  template = LeadTemplate.where(:name => template_name).first
   values.split(",").map{ |v| v.to_s.strip.split(":") }.each do |field_name, value|
     lead_template_field = template.lead_template_fields.detect { |ltf| ltf.name == field_name }
     LeadTemplateValue.create(:lead_template_field => lead_template_field, :value => value, :lead => lead)
@@ -44,7 +44,7 @@ Given /^template named "([^"]*)" for lead "([^"]*)" has values "([^"]*)"$/ do |t
 end
 
 Given /^template named "([^"]*)" is filled out by someone$/ do |name|
-  template = LeadTemplate.find_by_name(name)
+  template = LeadTemplate.where(:name => name).first
   template.lead_template_fields.create(:name => "Some field", :field_type => 0) if template.lead_template_fields.empty?
   template.reload
   LeadTemplateValue.create(:lead_template_field => template.lead_template_fields.first, :value => "Some value")
