af917885e490ff2c4ed861f0a9bdcb136b20b1d1
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-06-03 11:11:28 +0200

    #5975

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -50,6 +50,7 @@ class Lead < AbstractLead
   scope :without_bought_and_requested_by, lambda {|u| select("DISTINCT leads.*").joins("LEFT JOIN lead_purchases lp ON lp.lead_id = leads.id").where(["(lp.owner_id <> ? OR lp.owner_id IS NULL) AND (lp.assignee_id <> ? OR lp.assignee_id IS NULL) AND (lp.requested_by <> ? OR lp.requested_by IS NULL)", u.id, u.id, u.id]) if u}
   scope :bestsellers, order("lead_purchases_counter DESC")
   scope :latest, order("created_at DESC")
+  scope :contact_requests_for, lambda { |user| where("")}
   scope :interesting_for_user, lambda { |user| where("leads.category_id IN (?)", user.accessible_categories_ids) }
 
   scope :joins_on_lead_purchases , joins("INNER JOIN lead_purchases ON lead_purchases.lead_id=leads.id")
