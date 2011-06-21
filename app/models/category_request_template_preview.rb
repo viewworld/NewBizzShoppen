642afa98d509d81acf38d236e870dbaefa7f7471
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-06-21 10:49:07 +0200

    request category as guest

--- app/models/category_request_template_preview.rb
+++ app/models/category_request_template_preview.rb
@@ -12,7 +12,7 @@ class CategoryRequestTemplatePreview < EmailTemplatePreview
     self.can_be_contacted = params[:can_be_contacted].to_i == 1
     self.body = @email_template.render(options.merge({:category_name => category_name, :lead_description => lead_description, :phone_number => phone_number,
                                                       :leads_count_per_month => leads_count_per_month, :can_be_contacted => can_be_contacted ? "Yes" : "No",
-                                                     :request_type => user.has_any_role?(:agent, :call_centre_agent, :purchase_manager) ? "Agent" : "Buyer"}))
+                                                      :request_type => (user.nil? ? "Guest" : user.has_any_role?(:agent, :call_centre_agent, :purchase_manager) ? "Agent" : "Buyer")}))
     self.subject = @email_template.render_subject(options)
   end
 end
