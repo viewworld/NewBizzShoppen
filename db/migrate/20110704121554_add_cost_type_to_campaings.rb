a906082adb46fb5bec70c1277ce3a38275cf7bad
tgn <rb@rb-desktop.(none)>
2011-07-04 16:46:26 +0200

    #6604 cost calculation

--- db/migrate/20110704121554_add_cost_type_to_campaings.rb
+++ db/migrate/20110704121554_add_cost_type_to_campaings.rb
@@ -3,6 +3,8 @@ class AddCostTypeToCampaings < ActiveRecord::Migration
     add_column :campaigns, :cost_type, :integer
     add_column :campaigns, :fixed_cost_value, :float
     add_column :campaigns, :euro_fixed_cost_value, :float
+
+    ActiveRecord::Migration.execute "UPDATE campaigns SET cost_type = 3 WHERE cost_type IS NULL"
   end
 
   def self.down
