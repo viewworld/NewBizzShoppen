69d7b8c780d9fb7315da8691a027b4603b88c58c
tgn <rb@rb-desktop.(none)>
2011-07-07 14:14:07 +0200

    #6631 campaign report

--- app/controllers/callers/campaign_reports_controller.rb
+++ app/controllers/callers/campaign_reports_controller.rb
@@ -1,26 +1,48 @@
 class Callers::CampaignReportsController < Callers::CallerController
   inherit_resources
-  set_tab "campaigns"
+  set_tab "financial"
+  set_subtab "campaigns_reports"
 
-  before_filter lambda {authorize_role(:call_centre, :admin)}
+  before_filter lambda {authorize_role(:call_centre, :admin, :call_centre_agent, :agent)}
 
   def collection
     @date_from = params[:date_from] ? params[:date_from].to_date : Date.today-7
     @date_to = params[:date_to] ? params[:date_to].to_date : Date.today
     @per_user = params[:per_user].to_i == 1 ? true : false
+    @per_user = true if current_user.has_any_role?(:call_centre_agent, :agent)
+    @views_count = params[:views_count].to_i
 
     if current_user.has_role?(:admin)
       @campaigns = Campaign.where("")
-    elsif current_user.has_role?(:call_centre)
+    elsif current_user.has_any_role?(:call_centre, :call_centre_agent, :agent)
       @campaigns = Campaign.available_for_user(current_user)
     end
 
-    if @per_user
-      @campaign_users = current_user.has_role?(:admin) ?
-                          User.assigned_to_campaigns.with_results.with_agents_without_call_centres :
-                          User.assigned_to_campaigns.with_results.for_campaigns(@campaigns.map(&:id)).with_agents_without_call_centres.where("parent_id = ?", current_user.id)
+    @campaign_ids = params[:campaign_ids] || @campaigns.map(&:id)
+
+    @all_campaigns = @campaigns
+
+    unless @campaign_ids.size == @campaigns.size
+      @campaigns = !@campaign_ids.blank? ? @campaigns.where(:id => @campaign_ids) : @campaigns
     end
 
-    @campaign_reports = @campaigns.map { |campaign| CampaignReport.new(campaign, @date_from, @date_to) }
+    if @views_count > 0
+      if @per_user
+        @campaign_users = if current_user.has_role?(:admin)
+                            User.assigned_to_campaigns.with_results.with_agents_without_call_centres
+                          elsif current_user.has_role?(:call_centre)
+                            User.assigned_to_campaigns.with_results.for_campaigns(@campaigns.map(&:id)).with_agents_without_call_centres.where("parent_id = ?", current_user.id)
+                          elsif current_user.has_any_role?(:call_centre_agent, :agent)
+                            [current_user]
+                          end
+
+        @campaign_reports = @campaign_users.map { |user| @campaigns.map { |campaign| CampaignReport.new(campaign, @date_from, @date_to, user) } }.flatten
+      else
+        @campaign_reports = @campaigns.map { |campaign| CampaignReport.new(campaign, @date_from, @date_to) }
+      end
+    else
+      @campaign_reports = []
+      @campaign_users = []
+    end
   end
 end
