40ffcc4e74ba6f30ed9fe9be0c116a4233535c17
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-07-18 16:25:27 +0200

    rating

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -8,4 +8,13 @@ class DealsController < ApplicationController
     @category = Category.find_by_id(@search.with_category)
     @deals = @search.paginate(:page => params[:page], :per_page => Settings.default_leads_per_page)
   end
+
+  def rate
+    @deal = Deal.find(params[:id])
+    @deal.rate(params[:stars], current_user, params[:dimension])
+    render :update do |page|
+      page.replace_html @deal.wrapper_dom_id(params), ratings_for(@deal, params.merge(:wrap => false))
+      page.visual_effect :highlight, @deal.wrapper_dom_id(params)
+    end
+  end
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < AbstractLead
-
+  ajaxful_rateable :stars => 5, :dimensions => [:rating]
 
   scope :with_category, lambda { |q| where(:category_id => Category.find_by_id(q).self_and_descendants.map(&:id)) }
 
