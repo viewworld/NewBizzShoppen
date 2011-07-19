343d15a3ac3678bbc0c9a8beb8911103cde686f1
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-07-19 12:12:14 +0200

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Nbs::Application.configure do
   config.serve_static_assets               = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://static-fairleads.s3.amazonaws.com"
+  #config.action_controller.asset_host = "http://static-fairleads.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
