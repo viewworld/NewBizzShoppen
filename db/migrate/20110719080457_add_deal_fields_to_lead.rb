bfcc7f4f0529288fa69f13cfddc66c5a88566f1f
is <is@is-selleo.(none)>
2011-07-19 14:08:48 +0200

    deals assets

---
+++ db/migrate/20110719080457_add_deal_fields_to_lead.rb
@@ -0,0 +1,17 @@
+class AddDealFieldsToLead < ActiveRecord::Migration
+  def self.up
+    add_column :leads, :fine_print, :text
+    add_column :leads, :company_description, :text
+    add_column :leads, :start_date, :date
+    add_column :leads, :end_date, :date
+    add_column :leads, :deal_url, :string
+  end
+
+  def self.down
+    remove_column :leads, :deal_url
+    remove_column :leads, :end_date
+    remove_column :leads, :start_date
+    remove_column :leads, :company_description
+    remove_column :leads, :fine_print
+  end
+end
