f771641f6dcb228ad2598ca287432773cd28efe0
tgn <rb@rb-desktop.(none)>
2011-07-25 11:23:31 +0200

    #6944 deals - call centre agent

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -6,6 +6,7 @@ class Deal < AbstractLead
   has_many :materials, :class_name => "Asset::DealMaterial", :as => :resource, :conditions => "asset_type = 'Asset::DealMaterial'", :dependent => :destroy
   has_many :leads, :class_name => "Lead", :foreign_key => "deal_id"
   has_and_belongs_to_many :deal_templates, :class_name => "LeadTemplate", :join_table => "leads_lead_templates", :foreign_key => "lead_id", :association_foreign_key => "lead_template_id"
+  belongs_to :lead_category, :class_name => "Category", :foreign_key => "lead_category_id"
 
   scope :without_inactive, where("leads.end_date >= ? and leads.start_date <= ?", Date.today, Date.today)
   scope :without_requested_by, lambda { |u| select("DISTINCT leads.*").joins("LEFT JOIN leads lr ON lr.deal_id = leads.id").where(["(lr.requested_by <> ? OR lr.requested_by IS NULL)", u.id]) if u }
