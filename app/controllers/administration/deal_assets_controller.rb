983c8d2841d358f5a41092574ecf2a42aaa56e3c
is <is@is-selleo.(none)>
2011-07-25 11:48:17 +0200

    translations + admin deals listing

---
+++ app/controllers/administration/deal_assets_controller.rb
@@ -0,0 +1,13 @@
+class Administration::DealAssetsController < Administration::AdministrationController
+  before_filter :set_deal
+  before_filter :set_asset, :except => :create
+
+  include ::DealAssetActions
+
+  private
+
+  def path
+    edit_administration_deal_path(@deal)
+  end
+
+end
