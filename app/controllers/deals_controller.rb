ead828200241872a6fe78598323bf7517b4f1d4c
tgn <rb@rb-desktop.(none)>
2011-07-27 12:20:55 +0200

    #6991 deals - procurement manager changes

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -5,7 +5,6 @@ class DealsController < ApplicationController
 
   def collection
     @search = Deal.scoped_search(params[:search])
-    @search.without_requested_by = current_user
     @search.published_only = true
     @search.without_inactive = true
 
