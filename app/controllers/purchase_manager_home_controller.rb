6fd1fbb6e386dd3d001ce40a8b33f429601093b4
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-08-04 10:45:38 +0200

    m16

--- app/controllers/purchase_manager_home_controller.rb
+++ app/controllers/purchase_manager_home_controller.rb
@@ -17,7 +17,7 @@ class PurchaseManagerHomeController < ApplicationController
     end
 
     if user_signed_in? and current_user.has_role?(:purchase_manager)
-      @my_contact_requests = Lead.without_inactive.published_only.with_agent_unique_categories(current_user.id).contact_requests_for(current_user.id).limit(3)
+      @my_contact_requests = Lead.with_agent_unique_categories(current_user.id).contact_requests_for(current_user.id).limit(3)
     elsif user_signed_in?
       @latest_leads = Lead.published_only.without_bought_and_requested_by(current_user)
       @latest_leads = (current_user.has_any_role?(:agent, :call_centre_agent) ? @latest_leads.with_agent_unique_categories(current_user.id) : current_user.has_accessible_categories? ? @best_sellers.within_accessible_categories(current_user.accessible_categories_ids) : @best_sellers.with_customer_unique_categories(current_user.id)).latest.limit(3)
--- app/controllers/purchase_managers/leads_controller.rb
+++ app/controllers/purchase_managers/leads_controller.rb
@@ -11,15 +11,13 @@ class PurchaseManagers::LeadsController < PurchaseManagers::PurchaseManagerContr
 
   def collection
     if current_user
-      @categories = current_user.has_accessible_categories? ? LeadCategory.with_leads.within_accessible(current_user).without_locked_and_not_published : current_user.has_role?(:customer) ? LeadCategory.with_leads.without_locked_and_not_published.with_customer_unique(current_user) : LeadCategory.with_leads.without_locked_and_not_published.with_agent_unique(current_user)
+      @categories = current_user.has_accessible_categories? ? LeadCategory.with_leads.within_accessible(current_user).without_locked : current_user.has_role?(:customer) ? LeadCategory.with_leads.without_locked.with_customer_unique(current_user) : LeadCategory.with_leads.without_locked.with_agent_unique(current_user)
     else
       @categories = LeadCategory.with_leads.without_locked_and_not_published.without_unique
     end
 
     params[:search] ||= {}
     @search = Lead.scoped_search(params[:search])
-    @search.without_inactive = true if params[:search][:without_inactive].nil?
-    @search.without_outdated = true if params[:search][:without_outdated].nil?
     @leads = @search.where(:requested_by => current_user.id).paginate(:page => params[:page], :per_page => Settings.default_leads_per_page)
   end
 
