234bd89ef04b2db00827bd39afeb29b72ae5a7b8
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-08-18 12:50:12 +0200

    main page

--- app/controllers/fairdeals_home_controller.rb
+++ app/controllers/fairdeals_home_controller.rb
@@ -3,7 +3,8 @@ class FairdealsHomeController < ApplicationController
   set_tab "home"
 
   def show
-    @news         = Article::News::PurchaseManager.published.latest.limit(3)
+    @primary_deal = Deal.where(:id => params[:id]).first || FeaturedDeal.primary
+    @news = Article::News::PurchaseManager.published.latest.limit(3)
   end
 
 end
