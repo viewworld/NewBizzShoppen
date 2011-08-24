ab2ca670cd73ac905587b307465f154217fccd14
tgn <rb@rb-desktop.(none)>
2011-08-24 16:57:08 +0200

    mailer refactored

--- app/models/user.rb
+++ app/models/user.rb
@@ -198,7 +198,7 @@ class User < ActiveRecord::Base
   end
 
   def deliver_email_template(uniq_id)
-    ApplicationMailer.delay.email_template(email, uniq_id.to_sym, Country.get_country_from_locale, {:user => self})
+    ApplicationMailer.delay.email_template(email, uniq_id.to_sym, with_role.address.present? ? with_role.address.country : Country.get_country_from_locale, {:user => self.with_role})
   end
 
   def check_billing_rate
@@ -537,7 +537,7 @@ class User < ActiveRecord::Base
 
   def category_buyer_category_home_url
     if has_role?(:category_buyer)
-      "https://#{mailer_host}/#{buying_categories.first.cached_slug}"
+      "https://#{mailer_host}/#{with_role.buying_categories.first.cached_slug}"
     end
   end
 
