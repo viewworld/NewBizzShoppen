7d54c999c9c86425b7dc6774b5187538f203bfc7
tgn <rb@rb-desktop.(none)>
2011-09-16 13:32:48 +0200

    #7449 frontpage box - request a deal

--- app/controllers/deal_requests_controller.rb
+++ app/controllers/deal_requests_controller.rb
@@ -1,11 +1,10 @@
 class DealRequestsController < ApplicationController
 
   def create
-    @email_preview = DealRequestTemplatePreview.new('deal_request_for_deal_admin', params[:email_deal_request_preview], current_user, {:country => Country.get_country_from_locale})
-    throw @email_preview
+    @email_preview = DealRequestTemplatePreview.new('deal_request', params[:email_from], {:description => params[:body], :name => params[:name], :country => Country.get_country_from_locale})
     if @email_preview.valid?
       flash[:notice] = I18n.t("share_deal_by_email.create.flash.email_sent")
-      TemplateMailer.delay.new(Settings.default_deal_admin_email, :blank_template, Country.get_country_from_locale,
+      TemplateMailer.delay.new(params[:email_to], :blank_template, Country.get_country_from_locale,
                                        {:subject_content => @email_preview.subject, :body_content => @email_preview.body,
                                         :bcc_recipients => @email_preview.bcc, :cc_recipients => @email_preview.cc, :reply_to => @email_preview.email_from})
     end
