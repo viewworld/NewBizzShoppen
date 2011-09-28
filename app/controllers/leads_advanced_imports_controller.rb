2571a69a55a19ce9a9b8b5f65e6e2982a5a9bc1c
tgn <rb@rb-desktop.(none)>
2011-09-28 11:12:43 +0200

    #7835 import cookie overflow for too many errors

--- app/controllers/leads_advanced_imports_controller.rb
+++ app/controllers/leads_advanced_imports_controller.rb
@@ -13,7 +13,7 @@ class LeadsAdvancedImportsController < SecuredController
   end
 
   def show
-    @categories = LeadCategory.without_locked.with_agent_unique(params[:user_id] ? User.find_by_id(params[:user_id]) : current_user).without_customer_unique.all
+    @categories = LeadCategory.without_locked.with_agent_unique(params[:user_id] ? User.find_by_id(params[:user_id]) : current_user).all
 
     respond_to do |format|
       format.html { }
