88570f88913a8373087ebda3916aab2ceff41d57
tgn <rb@rb-desktop.(none)>
2011-10-10 09:53:14 +0200

    correction

--- app/controllers/suppliers/supplier_controller.rb
+++ app/controllers/suppliers/supplier_controller.rb
@@ -1,10 +1,15 @@
 class Suppliers::SupplierController < SecuredController
+  before_filter :authorize_user_for_namespace!
 
   def current_user
     @supplier ||= ::User.find_by_id(super.id)
   end
 
   private
+
+  def authorize_user_for_namespace!
+    authorize_role(:lead_supplier, :supplier)
+  end
 
   def authorize_for_team_buyers
     raise CanCan::AccessDenied unless current_user.team_buyers?
