e92935be7718758cf992d01ea8b05a8de215b0eb
tgn <rb@rb-desktop.(none)>
2011-11-18 14:02:51 +0100

    #7667 test send email

--- lib/string_utils.rb
+++ lib/string_utils.rb
@@ -3,4 +3,8 @@ class StringUtils
   def self.replace(str, old_phrase, new_phrase)
     str.gsub(/(#{old_phrase})/i) { |s| "#{ s.to_s[0..0] =~ /[A-Z]/ ? new_phrase.capitalize : new_phrase.downcase }" }
   end
+
+  def self.replace_template_variables(string)
+    string.gsub(/(\{\{[\s\w.]+\}\})+/i) { |s| s.gsub('{{', '[').gsub('}}', ']').gsub(/[._]/, "-").gsub(/\s/, '') }
+  end
 end
