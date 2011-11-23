7557d7a9aa4c6eef42b8dec6128f5f4739f63c10
Adrian <raven@raven-HP-Compaq-dx2400-Microtower.(none)>
2011-11-23 15:16:25 +0100

    m24

---
+++ db/migrate/20111123131647_add_fields_to_subscription_plan.rb
@@ -0,0 +1,34 @@
+class AddFieldsToSubscriptionPlan < ActiveRecord::Migration
+  def self.up
+    add_column :subscription_plans, :subscription_period, :integer
+    execute "UPDATE subscription_plans SET subscription_period = billing_cycle"
+
+    add_column :subscriptions, :subscription_period, :integer
+    execute "UPDATE subscriptions SET subscription_period = billing_cycle"
+
+    add_column :subscription_plans, :free_deals_in_free_period, :integer, :default => 0
+    add_column :subscriptions, :free_deals_in_free_period, :integer, :default => 0
+
+    add_column :subscription_plans, :use_paypal, :boolean, :default => false
+    add_column :subscriptions, :use_paypal, :boolean, :default => false
+
+    add_column :subscriptions, :paypal_retries, :integer, :default => false
+    add_column :subscription_plans, :paypal_retries, :integer, :default => true
+
+    add_column :subscription_plans, :paypal_billing_at_start, :boolean, :default => true
+
+    add_column :subscriptions, :paypal_billing_at_start, :boolean, :default => true
+
+    add_column :subscription_plans, :paypal_billing_at_end_of_free_period, :boolean, :default => false
+    add_column :subscriptions, :paypal_billing_at_end_of_free_period, :boolean, :default => false
+
+    add_column :subscription_plans, :automatic_downgrading, :boolean, :default => false
+    add_column :subscriptions, :automatic_downgrading, :boolean, :default => false
+
+    add_column :subscription_plans, :automatic_downgrade_subscription_plan_id, :boolean, :default => false
+    add_column :subscriptions, :automatic_downgrade_subscription_plan_id, :boolean, :default => false
+  end
+
+  def self.down
+  end
+end
