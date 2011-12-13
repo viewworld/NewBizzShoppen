bb2bb68f20faa8a96875f293851f9045927b9869
tgn <rb@rb-desktop.(none)>
2011-12-13 11:08:51 +0100

    members error

--- app/controllers/members/member_controller.rb
+++ app/controllers/members/member_controller.rb
@@ -1,6 +1,7 @@
 class Members::MemberController < SecuredController
   def current_user
     @agent ||= ::User::Member.find_by_id(super.id)
+    @agent ||= User.find_by_id(super.id).with_role
   end
 
   private
