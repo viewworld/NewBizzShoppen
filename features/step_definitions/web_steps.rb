348bb503b0d92797d96dc990be6c14d970e7824e
tgn <rb@rb-desktop.(none)>
2012-01-16 16:58:02 +0100

    tests

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -452,3 +452,7 @@ end
 When /^I click div "([^"]*)"$/ do |id|
   page.first(:css, "div[id*='#{id}']").click()
 end
+When /^I set date "([^\"]*)" for field "([^\"]*)"$/ do |date, date_field|
+  date = date.to_s.include?("Date") ? eval(date) : date
+  Then %{I fill in "#{date_field}" with "#{date}"}
+end
