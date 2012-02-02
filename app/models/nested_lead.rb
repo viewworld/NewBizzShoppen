f582e8375cefcc6152002fd41140a29eebb293a8
tgn <rb@rb-desktop.(none)>
2012-02-02 09:33:17 +0100

    #9995 get deals during upgrade of contact to member / corrections 4

--- app/models/nested_lead.rb
+++ app/models/nested_lead.rb
@@ -14,7 +14,7 @@ class NestedLead
     if params
       self.user = User::Member.find(params[:user_id])
       params[:nested_lead][:leads_attributes].each_pair do |key, attributes|
-        lead = Lead.new(attributes)
+        lead = Lead.new(attributes.merge(:dont_send_email_with_deal_details_and_files => true))
         lead.based_on_deal(lead.deal, user)
         self.leads << lead
       end
