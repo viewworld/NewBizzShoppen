264abe2dbe6bd51a7de77800221954c7d3d62c35
tgn <rb@rb-desktop.(none)>
2012-02-03 09:59:42 +0100

    #9889 dynamic result value / interface translations and tests

--- app/models/campaigns_result_field.rb
+++ app/models/campaigns_result_field.rb
@@ -3,8 +3,16 @@ class CampaignsResultField < ActiveRecord::Base
   belongs_to :result_field
 
   validate :euro_value_present_if_is_dynamic_value
+  before_save :disable_dynamic_value_if_not_number
 
   private
+
+  def disable_dynamic_value_if_not_number
+    if is_dynamic_value? and result_field.field_type.to_i != ResultField::INTEGER
+      self.is_dynamic_value = false
+    end
+    true
+  end
 
   def euro_value_present_if_is_dynamic_value
     if is_dynamic_value? and dynamic_euro_value.blank?
