b354045afe4161836ef1296705a1cbcc755402d2
aossowski <aossowski@gmail.com>
2012-03-26 13:25:21 +0200

    deal saving

--- features/system/newsletter_backend.feature
+++ features/system/newsletter_backend.feature
@@ -16,6 +16,7 @@ Feature: Newsletter backend
   @_done @_tested_elsewhere @_rspec
   Scenario: When a new list is created then the subscribers are exported from the sources to Campaign Monitor
 
+  @_done @_tested_elsewhere @_rspec
   Scenario: When a source of the list is updated (new contact, new user or new lead) then is should be synced with Campaign Monitor
 
   Scenario: When a source of the list is removed from the list then the subscribers should be removed from the list in Campaign Monitor (it applies also to tags that may be the source)
--- spec/models/newsletter_list_spec.rb
+++ spec/models/newsletter_list_spec.rb
@@ -33,7 +33,7 @@ describe NewsletterList do
     @list.owner.cm_client_id.should == "ClientId10123123"
   end
 
-  it "export all subscribers from sources when new list is created" do
+  it "should export all subscribers from sources when new list is created" do
     CreateSend::List.expects(:create).returns("List1023456")
     @lead = Lead.make!(:category => @lead_category)
     @list = NewsletterList.make
@@ -41,6 +41,42 @@ describe NewsletterList do
     @list.save
     CreateSend::Subscriber.expects(:add).with("List1023456", @lead.email_address, @lead.contact_name, [], true)
     Delayed::Worker.new.work_off
+  end
+
+  it "should export all new subscribers in sources" do
+    CreateSend::List.expects(:create).times(3).returns("List#{rand(999)}")
+
+    @list_lead_category = NewsletterList.make!
+    @list_lead_category.newsletter_sources.create(:source_type => NewsletterSource::LEAD_CATEGORY_SOURCE, :sourceable => @lead_category)
+
+    @campaign = Campaign.make!
+    @list_campaign = NewsletterList.make!
+    @list_campaign.newsletter_sources.create(:source_type => NewsletterSource::CAMPAIGN_SOURCE, :sourceable => @campaign)
+
+    @role = Role.find(4)
+    @list_role = NewsletterList.make!
+    @list_role.newsletter_sources.create(:source_type => NewsletterSource::USER_ROLE_SOURCE, :sourceable => @role)
+
+    @lead = Lead.make!(:category => @lead_category)
+    CreateSend::Subscriber.expects(:add).with(@list_lead_category.cm_list_id, @lead.email_address, @lead.contact_name, [], true)
+
+    @contact = Contact.make!(:campaign => @campaign)
+    CreateSend::Subscriber.expects(:add).with(@list_campaign.cm_list_id, @contact.email_address, @contact.contact_name, [], true)
+
+    @member = User::Member.make!
+    CreateSend::Subscriber.expects(:add).with(@list_role.cm_list_id, @member.email, @member.full_name, [], true)
+
+    Delayed::Worker.new.work_off
+  end
+
+  it "should remove subscriber from campaign monitor when object is deleted from sources" do
+    CreateSend::List.expects(:create).once().returns("List123")
+    @list_lead_category = NewsletterList.make!
+    @list_lead_category.newsletter_sources.create(:source_type => NewsletterSource::LEAD_CATEGORY_SOURCE, :sourceable => @lead_category)
+    @lead = Lead.make!(:category => @lead_category)
+    CreateSend::Subscriber.expects(:add).with(@list_lead_category.cm_list_id, @lead.email_address, @lead.contact_name, [], true)
+    Delayed::Worker.new.work_off
+
   end
 
 end
