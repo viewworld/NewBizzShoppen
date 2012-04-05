a14248e9e3daa4556aef4ca333f7f0443975f1bd
tnoworyta <tomasz.noworyta@gmail.com>
2012-04-05 15:28:07 +0200

    #10704 newsletter / tests

--- spec/models/newsletter_synch_spec.rb
+++ spec/models/newsletter_synch_spec.rb
@@ -10,6 +10,8 @@ describe NewsletterSynch do
 
   it "should export all new subscribers in sources" do
     CreateSend::List.stubs(:create).times(3).returns("List1","List2","List3")
+    CreateSend::List.any_instance.stubs(:create_custom_field).returns(true)
+    CreateSend::List.any_instance.stubs(:details).returns(false).then.returns(false).then.returns(false).then.returns(Hashie::Mash.new(:ListID => "List1"), Hashie::Mash.new(:ListID => "List2"), Hashie::Mash.new(:ListID => "List3"))
 
     @list_lead_category = NewsletterList.make!
     @list_lead_category.newsletter_sources.create(:source_type => NewsletterSource::LEAD_CATEGORY_SOURCE, :sourceable => @lead_category)
@@ -23,13 +25,13 @@ describe NewsletterSynch do
     @list_role.newsletter_sources.create(:source_type => NewsletterSource::USER_ROLE_SOURCE, :sourceable => @role)
 
     @lead = Lead.make!(:category => @lead_category)
-    CreateSend::Subscriber.stubs(:add).with(@list_lead_category.cm_list_id, @lead.email_address, @lead.contact_name, [], true)
+    CreateSend::Subscriber.stubs(:add).with(@list_lead_category.cm_list_id, @lead.email_address, @lead.contact_name, [], true).once
 
     @contact = Contact.make!(:campaign => @campaign)
-    CreateSend::Subscriber.stubs(:add).with(@list_campaign.cm_list_id, @contact.email_address, @contact.contact_name, [], true)
+    CreateSend::Subscriber.stubs(:add).with(@list_campaign.cm_list_id, @contact.email_address, @contact.contact_name, [], true).once
 
     @member = User::Member.make!
-    CreateSend::Subscriber.stubs(:add).with(@list_role.cm_list_id, @member.email, @member.full_name, [], true)
+    CreateSend::Subscriber.stubs(:add).with(@list_role.cm_list_id, @member.email, @member.full_name, [], true).once
 
     Delayed::Worker.new.work_off
   end
@@ -37,6 +39,7 @@ describe NewsletterSynch do
   it "should remove subscriber from campaign monitor when object is deleted from sources" do
     CreateSend::List.any_instance.stubs(:details).returns(false)
     CreateSend::List.stubs(:create).returns("List1023456")
+    CreateSend::List.any_instance.stubs(:create_custom_field).returns(true)
     @lead = Lead.make!(:category => @lead_category)
     CreateSend::List.any_instance.stubs(:active).returns(Hashie::Mash.new(:RecordsOnThisPage => 0, :Results => [])).then.returns(Hashie::Mash.new(:RecordsOnThisPage => 1, :Results => [{:EmailAddress => @lead.email_address}]))
     CreateSend::Subscriber.stubs(:import).twice()
