171465bbf0d93b570875927cc22a9cc23dcc5f78
tnoworyta <tomasz.noworyta@gmail.com>
2012-05-10 12:57:01 +0200

    #11115 quickpay integration / confirm subscription

--- app/models/active_merchant_subscription_payment_notification.rb
+++ app/models/active_merchant_subscription_payment_notification.rb
@@ -4,7 +4,7 @@ class ActiveMerchantSubscriptionPaymentNotification < SubscriptionPaymentNotific
     subscription_id = params[:ordernumber].split("_").last
     active_subscription = Subscription.find_by_id(subscription_id)
     subscription_plan = SubscriptionPlan.find_by_id(params[:target_subscription_plan_id])
-    payment_notification = ActiveMerchantSubscriptionPaymentNotification.create!(:params => params, :buyer_id => subscription.user_id, :status => TRANSACTION_STATUSES[params[:qpstat]], :transaction_id => params[:transaction])
+    payment_notification = ActiveMerchantSubscriptionPaymentNotification.create!(:params => params, :buyer_id => active_subscription.user_id, :status => TRANSACTION_STATUSES[params[:qpstat]], :transaction_id => params[:transaction])
 
     if payment_notification.status == TRANSACTION_STATUSES[APPROVED] && params[:md5check] == payment_notification.calculate_md5_check(params, :window_response) &&
             params[:merchantemail] == APP_CONFIG[:quickpay_email]
