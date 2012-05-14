5b23ecf2bae76180a96d46690c41dfe6d13f3dc0
tnoworyta <tomasz.noworyta@gmail.com>
2012-05-14 13:40:50 +0200

    #11115 quickpay integration / payment for subperiods

--- app/models/active_merchant_recurring_payment.rb
+++ app/models/active_merchant_recurring_payment.rb
@@ -9,7 +9,7 @@ class ActiveMerchantRecurringPayment
 
   def make_payment!
     return true if @subperiod.payment_paid?
-    @result = @gateway.purchase((@subperiod.billing_price * 100).to_i, @subperiod.subscription.payment_profile_id, :order_id => "ssp#{@subperiod.subscription.id}_#{@subperiod.id}")
+    @result = @gateway.purchase((@subperiod.billing_price * 100).to_i, @subperiod.subscription.payment_profile_id, :order_id => "ssp#{@subperiod.id}")
 
     if @result.success?
       @subperiod.update_attributes(:payment_paid_auto => true, :payment_retry_at => nil)
