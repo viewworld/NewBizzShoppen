da225dfeb718db3a0c716502bfcbd70bd9d3e0f4
tnoworyta <tomasz.noworyta@gmail.com>
2012-05-14 13:59:01 +0200

    #11115 quickpay integration / payment for subperiods

--- app/models/active_merchant_recurring_payment.rb
+++ app/models/active_merchant_recurring_payment.rb
@@ -13,7 +13,8 @@ class ActiveMerchantRecurringPayment
 
     if @result.success?
       @subperiod.update_attributes(:payment_paid_auto => true, :payment_retry_at => nil)
-      if @subperiod.subscription.subscription_sub_periods.last == @subperiod
+      if @subperiod.subscription.subscription_sub_periods.last == @subperiod and
+          @subperiod.subscription.subscription_sub_periods.where("payment_paid_auto is false and payment_paid_manual is false").empty?
         ActiveMerchantRecurringProfile.new(@subperiod.subscription.payment_profile_id).cancel_profile
       end
     else
