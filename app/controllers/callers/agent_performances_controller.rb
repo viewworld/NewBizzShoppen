4940a57beaf6f393b11949c370f98e979d8b71e9
raven <aossowski@gmail.com>
2013-04-11 20:19:45 +0200

    #14972

--- app/controllers/callers/agent_performances_controller.rb
+++ app/controllers/callers/agent_performances_controller.rb
@@ -4,6 +4,7 @@ class Callers::AgentPerformancesController < Callers::CallerController
   set_subtab "agent_performance"
 
   def show
+    params[:search] ||= {}
     @search = AgentPerformance::Search.new(params[:search].merge(:current_user => current_user))
     @search.to_file
   end
