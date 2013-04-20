39711c51adc74f6034654f3edefa6bb7420658fe
raven <aossowski@gmail.com>
2013-04-20 11:20:04 +0200

    #15603

--- app/controllers/administration/home_controller.rb
+++ app/controllers/administration/home_controller.rb
@@ -6,7 +6,7 @@ class Administration::HomeController < ApplicationController
     @campaigns = Campaign.available_for_user(current_user).with_state("active")
     params[:date_from] ||= Date.today
     params[:date_to] ||= Date.today
-    params[:currency_id] ||= Currency.active.first.id
+    params[:currency_id] ||= Currency.dkk
     @performance_campaigns = Campaign.active_between(params[:date_from], params[:date_to]).available_for_user(current_user)
     @agents = User.select("DISTINCT(users.*)").joins(:user_session_logs).where(:user_session_logs => { :campaign_id => @performance_campaigns, :end_date => params[:date_from]..params[:date_to] })
   end
