70eed23a29280645d3f887eb03d0f48d234a86f4
raven <aossowski@gmail.com>
2013-04-26 11:56:35 +0200

    recorded calls as mp4

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -29,13 +29,21 @@ module CampaignsHelper
       when ResultField::MATERIAL then result.materials.map{|material| link_to(material.asset_file_name, material.url) }.join(", ").html_safe
       else result.value
     end
+  end
 
+  def call_url(call_id)
+    @calls ||= Dir.entries(File.join(Rails.root,'public/system/calls'))
+    if filename = @calls.detect{|f| f.match(/#{call_id}\.(mp4|wav)$/)}
+      "/system/calls/#{filename}"
+    else
+      nil
+    end
   end
 
   def link_to_call(call_id)
     @calls ||= Dir.entries(File.join(Rails.root,'public/system/calls'))
-    if filename = @calls.detect{|f| f.match(/#{call_id}\.(aac|wav)$/)}
-      bt_link_to :download, filename[/(aac|wav)$/,1], "/system/calls/#{filename}"
+    if fileurl = call_url(call_id)
+      bt_link_to :download, fileurl[/(mp4|wav)$/,1], fileurl
     end
   end
 
