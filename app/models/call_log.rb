45752eba6513720d25c7c2498e3ce5569155de9c
raven <aossowski@gmail.com>
2013-04-29 12:58:46 +0200

    call log fix

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -2,7 +2,7 @@ class CallLog < ActiveRecord::Base
 
   belongs_to :contact
   belongs_to :campaign
-  belongs_to :caller, :polymorphic => true
+  #belongs_to :caller, :polymorphic => true
 
   scope :for_campaigns, lambda { |campaign_ids| where("call_logs.campaign_id in (?)", campaign_ids.to_a) unless campaign_ids.to_a.empty? }
   scope :with_state, lambda { |state| where(:state => state) }
