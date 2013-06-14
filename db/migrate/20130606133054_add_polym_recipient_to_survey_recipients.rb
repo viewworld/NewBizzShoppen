7f6968cd334efe360ff0a017976d57d192408ddc
tnoworyta <tomasz.noworyta@gmail.com>
2013-06-14 14:08:59 +0200

    #15122 new result type- survey -- final corrections

--- db/migrate/20130606133054_add_polym_recipient_to_survey_recipients.rb
+++
@@ -1,9 +0,0 @@
-class AddPolymRecipientToSurveyRecipients < ActiveRecord::Migration
-  def self.up
-    add_column :survey_recipients, :recipient_id, :integer
-    add_column :survey_recipients, :recipient_type, :string
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20130610063809_add_survey_id_to_result_fields.rb
+++
@@ -1,8 +0,0 @@
-class AddSurveyIdToResultFields < ActiveRecord::Migration
-  def self.up
-    add_column :result_fields, :survey_id, :integer, :default => nil
-  end
-
-  def self.down
-  end
-end
