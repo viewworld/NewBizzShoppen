c051ed8ae698ac690dcf044384e2052dd3285922
tnoworyta <tomasz.noworyta@gmail.com>
2013-07-15 14:48:59 +0200

    FL-59 capcha for request deal on fairdeals main page

--- app/controllers/deal_requests_controller.rb
+++ app/controllers/deal_requests_controller.rb
@@ -2,7 +2,8 @@ class DealRequestsController < ApplicationController
 
   def create
     @email_preview = DealRequestTemplatePreview.new('deal_request_for_deal_admin', params[:email_deal_request_preview], current_user, {:country => Country.get_country_from_locale})
-    if @email_preview.valid?
+    @captcha = verify_recaptcha(:message => t("share_deal_by_email.create.flash.invalid_captcha"))
+    if @email_preview.valid? and @captcha
       flash[:notice] = I18n.t("deal_requests.create.flash.email_sent")
       TemplateMailer.new(Settings.default_deal_admin_email, :blank_template, Country.get_country_from_locale,
                                        {:subject_content => @email_preview.subject, :body_content => @email_preview.body,
@@ -10,6 +11,5 @@ class DealRequestsController < ApplicationController
                                         :reply_to => @email_preview.email_from, :sender_id => user_signed_in? ? current_user.id : nil,
                                         :email_template_uniq_id => "deal_request_for_deal_admin", :email_template_id => @email_preview.email_template_id}).deliver!
     end
-    redirect_to :back
   end
 end
