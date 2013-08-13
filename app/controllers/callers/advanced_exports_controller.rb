3b87e25d374e3c3cffc8145862580adc72b6f8c6
raven <aossowski@gmail.com>
2013-08-13 22:03:13 +0200

    FL-109

--- app/controllers/callers/advanced_exports_controller.rb
+++ app/controllers/callers/advanced_exports_controller.rb
@@ -1,13 +1,21 @@
 class Callers::AdvancedExportsController < Callers::CallerController
   set_tab "campaigns"
+  include ::CampaignActions
 
   before_filter :set_campaign
   before_filter lambda { authorize_role(:call_centre, :admin) }
 
   def new
+    @advanced_export = AdvancedExport.new(params)
   end
 
   def create
+    set_locals(params[:advanced_export])
+    @call_result_ids = CallResult.for_table_row(@date_from, @date_to, @results.map(&:id), @agent_ids, @campaign.is_a?(Array) ? @campaign.map(&:id) : @campaign.id).map{|hash| hash[:ids]}.flatten
+
+    respond_to do |wants|
+      wants.xls { send_data render_to_string, :filename => "call_result.xls" }
+    end
   end
 
   private
