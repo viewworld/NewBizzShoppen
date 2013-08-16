7f2e025a7923ddd1e872cc80c3256853a6371fb0
raven <aossowski@gmail.com>
2013-08-16 17:18:19 +0200

    FL-111

---
+++ app/helpers/callers/user_session_logs_helper.rb
@@ -0,0 +1,19 @@
+module Callers::UserSessionLogsHelper
+
+  def link_to_user_session_logs(additional_conditions)
+    callers_user_session_logs_path(
+        :search => {
+            :with_time_from => "#{params[:date_from] || Date.today} 00:00",
+            :with_time_to => "#{params[:date_to] || Date.today} 23:59"
+        }.merge(additional_conditions))
+  end
+
+  def campaign_link_to_user_session_logs(campaign)
+    link_to_user_session_logs :for_campaign => campaign.id
+  end
+
+  def agent_link_to_user_session_logs(agent)
+    link_to_user_session_logs :for_user => agent.id, :with_log_type => 1
+  end
+
+end
