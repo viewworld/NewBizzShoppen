4b39cb70c43f538ebb4bcfe3e437269909d88b55
raven <aossowski@gmail.com>
2013-08-19 20:55:20 +0200

    FL-120

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -7,12 +7,13 @@ describe Notification do
     before(:each) do
     end
 
+    # notifications for agents temporarily disabled
     it "should dismiss all remaining notifications when contact is finalized" do
       @creator = User::Agent.make!
       @contact = Contact.make!
       CallResult.make!(:call_back, :contact => @contact, :creator => @creator)
-      @creator.notifications.pending.size.should == 1
-      @contact.notifications.pending.size.should == 1
+      @creator.notifications.pending.size.should == 0
+      @contact.notifications.pending.size.should == 0
 
       CallResult.make!(:upgraded_to_member, :contact => @contact, :creator => @creator)
       @contact.reload
