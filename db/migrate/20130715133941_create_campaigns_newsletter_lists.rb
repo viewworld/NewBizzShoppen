928a28b14f482c6cd196631f6739893669fe48bd
tnoworyta <tomasz.noworyta@gmail.com>
2013-09-23 13:34:31 +0200

    FL-56 use lists as campaign contacts - corrections

--- db/migrate/20130715133941_create_campaigns_newsletter_lists.rb
+++ db/migrate/20130715133941_create_campaigns_newsletter_lists.rb
@@ -4,9 +4,13 @@ class CreateCampaignsNewsletterLists < ActiveRecord::Migration
       t.integer :campaign_id
       t.integer :newsletter_list_id
     end
+
+    add_column :campaigns, :import_contacts_from_lists_enabled, :boolean, :default => false
+    execute "UPDATE campaigns SET import_contacts_from_lists_enabled = 'f' WHERE import_contacts_from_lists_enabled IS NULL"
   end
 
   def self.down
     drop_table :campaigns_newsletter_lists
+    remove_column :campaigns, :import_contacts_from_lists_enabled
   end
 end
