463a51807be4744dc03b4b6ac75e9beb68c2ef49
bart <wojtowicz.bartlomiej@gmail.com>
2013-11-27 16:24:39 +0100

    add migration for custom fields #fl-183 comment

---
+++ db/migrate/20131127144236_add_custom_fields_to_contact.rb
@@ -0,0 +1,34 @@
+class AddCustomFieldsToContact < ActiveRecord::Migration
+
+  TABLE_NAMES = [
+    :leads,
+    :newsletter_list_subscriber
+  ]
+
+  CUSTOM_COLUMN_NAMES = [
+    :pnumber,
+    :nnmid,
+    :custom_1,
+    :custom_2,
+    :custom_3,
+    :custom_4,
+    :custom_5
+  ]
+
+  def self.up
+    TABLE_NAMES.each do |table_name|
+      CUSTOM_COLUMN_NAMES.each do |column_name|
+        add_column table_name, :string, column_name
+      end
+    end
+  end
+
+  def self.down
+    TABLE_NAMES.each do |table_name|
+      CUSTOM_COLUMN_NAMES.each do |column_name|
+        remove_column table_name, column_name
+      end
+    end
+  end
+
+end
