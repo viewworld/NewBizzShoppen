c334c3af21edb2d4ae6b4dc5a0c8fe5fe9ecbc04
Bartłomiej Danek <bartek.danek@gmail.com>
2014-01-21 12:40:39 +0100

    staging config for unicorn

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,17 @@
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+
+worker_processes 4
+
+working_directory "/srv/nbs/staging/current"
+
+#listen "/srv/nbs/staging/shared/pids/staging.socket", :backlog => 64
+listen 30000, :backlog => 64
+
+timeout 30
+
+user 'rails', 'rails'
+shared_path = "/srv/nbs/staging/shared"
+pid "#{shared_path}/pids/unicorn.pid"
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
