5e79d7a0e02f618abeedcf1274466f43d45ee1fb
Bartłomiej Danek <bartek.danek@gmail.com>
2014-01-24 08:51:10 +0100

    deploy static content for staging cleanup deploy for staging

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,3 @@ after "deploy:finalize_update", "deploy_static_content"
 task :restart_memcached, :roles => :app do
   sudo "nohup /etc/init.d/memcached restart &"
 end
-
-task :deploy_static_content, :roles => :app do
-  run "cd #{release_path}; RAILS_ENV=#{stage} bundle exec thor i18n:js; bundle exec vendor/gems/jammit-s3/bin/jammit-s3 --base-url static-fairleads.s3.amazonaws.com"
-end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,4 @@ set :deploy_to, "/srv/#{application}/staging"
 set :rails_env, "staging"
 set :branch, "master"
 
-set :mailcatcher_smtp_ip, 'localhost'
-set :mailcatcher_smtp_port, 1025
-
-set :mailcatcher_http_ip, fetch(:server_ip)
-set :mailcatcher_http_port, 10801
+after "deploy:finalize_update", "deploy_static_content"
