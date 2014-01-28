1d2a7172b9382e0f17a8a965bf070fc3f7827829
Bartłomiej Danek <bartek.danek@gmail.com>
2014-01-28 10:42:10 +0100

    fix syntax for spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,18 +6,18 @@ require 'mocha/setup'
 FakeWeb.allow_net_connect = %r{(.*paypal\.com.*)$}
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
+  # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
 
   # This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+  # from the project root directory.
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
   RSpec.configure do |config|
@@ -31,10 +31,4 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-
 end
-
-
-
-
-
