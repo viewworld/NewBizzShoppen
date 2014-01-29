e3d51d9ddd73004107d40649a104321c8124dc53
Bartłomiej Danek <bartek.danek@gmail.com>
2014-01-29 20:01:05 +0100

    setup branch for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, "/srv/#{application}/production"
 set :rails_env, "production"
-set :branch, "master"
+set :branch, "staging"
 
 after "deploy:restart", "restart_memcached"
 after "deploy:finalize_update", "deploy_static_content"
