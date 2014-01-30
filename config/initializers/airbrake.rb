c6a8d82cc7879df30c6384767ad19429908d3577
Bartłomiej Danek <bartek.danek@gmail.com>
2014-01-30 15:59:28 +0100

    setup Errbit for staging/production

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,8 @@
-#Airbrake.configure do |config|
-#  config.api_key = 'c7294dd91b38dc8445409e7ebc8fa0f6'
-#  config.host    = 'errbit.selleo.com'
-#  config.port    = 443
-#  config.secure  = config.port == 443
-#end
+if %w(staging production).include?(Rails.env)
+  Airbrake.configure do |config|
+    config.api_key = 'c7294dd91b38dc8445409e7ebc8fa0f6'
+    config.host    = 'errbit.selleo.com'
+    config.port    = 443
+    config.secure  = config.port == 443
+  end
+end
