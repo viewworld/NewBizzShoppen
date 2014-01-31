b938fe0edadc33f2296008edb0b7919acab7600c
irek <irek.skrobis@gmail.com>
2014-01-31 13:17:56 +0100

    convert to correct currency and display it properly #FL-327 comment

--- app/models/pipeline_report.rb
+++ app/models/pipeline_report.rb
@@ -1,7 +1,17 @@
 class PipelineReport < ActiveRecord::Base
   belongs_to :user, :foreign_key => :owner_id
 
+  FIELDS_FOR_DISPLAY = %w(company_name lead_header contact_name decision_date state value estimate pipeline_value)
+
   def state_for_display
     I18n.t("lead_purchases.statuses.#{LeadPurchase::STATUSES[state]}")
+  end
+
+  def euro_value_for_currency(currency)
+    currency.from_euro(euro_value)
+  end
+
+  def euro_pipeline_value_for_currency(currency)
+    currency.from_euro(euro_pipeline_value)
   end
 end
--- app/models/pipeline_report_generator.rb
+++ app/models/pipeline_report_generator.rb
@@ -1,9 +1,11 @@
 class PipelineReportGenerator
   attr_reader :user, :order, :report_lines
+  attr_accessor :currency
 
   def initialize(user, order)
     @user = user
     @order = order
+    @currency = user.pipeline_report_currency ||= Currency.euro
     @report_lines = user.pipeline_reports.order(order_by_state? ? 'state, year, week, header' : "year, #{order}, header")
   end
 
@@ -11,12 +13,10 @@ class PipelineReportGenerator
     [].tap do |result|
       titles.each do |title|
         lines = lines(title)
-        result << {
-            :title => title,
-            :lines => lines,
-            :euro_value_sum => lines.sum(&:euro_value),
-            :euro_pipeline_value_sum => lines.sum(&:euro_pipeline_value)
-        }
+        result << {:title => title_for_display(title),
+                   :lines => lines_to_array_of_hash(lines),
+                   :value_sum => to_users_currency(lines.sum(&:euro_value)),
+                   :pipeline_value_sum => to_users_currency(lines.sum(&:euro_pipeline_value))}
       end
     end
   end
@@ -36,15 +36,36 @@ class PipelineReportGenerator
     report_lines.select { |report_line| report_line.year == title_array.last.to_i && report_line.send(order) == title_array.first.to_i }
   end
 
+  def lines_to_array_of_hash(lines)
+    lines.map do |line|
+      {:company_name => line.company_name,
+       :lead_header => line.header,
+       :contact_name => line.contact_name,
+       :decision_date => line.decision_date,
+       :state_for_display => line.state_for_display,
+       :value => line.euro_value_for_currency(currency),
+       :estimate => line.estimate,
+       :pipeline_value => line.euro_pipeline_value_for_currency(currency)}
+    end
+  end
+
   def order_by_state?
     @order_by_state ||= order == 'state'
   end
 
   def titles
-    @titles ||= report_lines.map { |report_line| title(report_line) }.uniq
+    @titles ||= report_lines.map { |report_line| title_from_report_line(report_line) }.uniq
   end
 
-  def title(report_line)
+  def title_for_display(title)
+    order_by_state? ? I18n.t("lead_purchases.statuses.#{LeadPurchase::STATUSES[title]}") : "#{order}: #{title}"
+  end
+
+  def title_from_report_line(report_line)
     order_by_state? ? report_line.state : "#{report_line.send(order)}, #{report_line.year}"
+  end
+
+  def to_users_currency(number)
+    currency.from_euro(number)
   end
 end
