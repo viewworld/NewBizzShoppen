802b9b4dba339cfae16f9d2dc3173d7e5b5f6724
irek <irek.skrobis@gmail.com>
2014-01-31 12:29:08 +0100

    translate status number #FL-327 comment

--- app/models/pipeline_report.rb
+++ app/models/pipeline_report.rb
@@ -1,3 +1,7 @@
 class PipelineReport < ActiveRecord::Base
   belongs_to :user, :foreign_key => :owner_id
+
+  def state_for_display
+    I18n.t("lead_purchases.statuses.#{LeadPurchase::STATUSES[state]}")
+  end
 end
