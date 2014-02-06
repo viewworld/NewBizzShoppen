620ce3126a1fa235e79cf70f41e3dce89e5aee0a
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-06 12:01:13 +0100

    use locales from YAML in test env

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,13 +1,15 @@
-# ActiveRecord backend
-require 'i18n/active_record'
-I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
-I18n.backend = I18n::Backend::ActiveRecord.new
-I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+unless Rails.env.test?
+  # ActiveRecord backend
+  require 'i18n/active_record'
+  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
+  I18n.backend = I18n::Backend::ActiveRecord.new
+  I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 
-# Fallbacks
-require "i18n/backend/fallbacks"
-I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Fallbacks)
-I18n.default_locale = :"en"
+  # Fallbacks
+  require "i18n/backend/fallbacks"
+  I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Fallbacks)
+  I18n.default_locale = :"en"
 
-I18n.fallbacks[:en] = [:en, :da]
-I18n.fallbacks[:da] = [:da, :en]
+  I18n.fallbacks[:en] = [:en, :da]
+  I18n.fallbacks[:da] = [:da, :en]
+end
