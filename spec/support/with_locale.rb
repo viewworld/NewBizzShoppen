c6c21eb58ad16b59989397832a0fba1e0ac53c0e
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-07 10:31:19 +0100

    helper with_locale for specs  #FL-356 comment

--- spec/support/with_locale.rb
+++ spec/support/with_locale.rb
@@ -1,4 +1,4 @@
-def with_locale(locale)
-  locales = Array[double(:to_s => locale, :code => locale, :symbol => locale)]
-  allow(Locale).to receive(:enabled).and_return(double(:all => locales))
-end
+# def with_locale(locale)
+#   locales = Array[double(:to_s => locale, :code => locale, :symbol => locale)]
+#   allow(Locale).to receive(:enabled).and_return(double(:all => locales))
+# end
