211304d3035d96a8abb10892f1398709cf0b00c0
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-14 11:49:39 +0100

    prepare shared_context for request specs #FL-398 comment

---
+++ spec/support/request_specs_context.rb
@@ -0,0 +1,19 @@
+shared_context 'request specs context' do
+  before do
+    Settings.big_supplier_purchase_limit = 10000
+    with_site('fairleads')
+    Locale.make!
+    VatRate.make!
+    without_confirmation_email!
+    without_invoice_email_template!
+    without_lead_notification!
+    admin.roles << :superadmin
+    admin.save!
+  end
+
+  let(:admin) { User::Admin.make!(:confirmed_at => Time.now) }
+  let(:seller) { Seller.make! }
+  let(:company_name) { 'CompanyName' }
+  let(:lead_category) { LeadCategory.find_by_name(company_name) }
+  let!(:currency) { Currency.make!(:name => 'EUR', :global_default => true) }
+end
