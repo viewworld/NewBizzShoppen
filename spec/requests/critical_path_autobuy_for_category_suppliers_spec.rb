d1fe946d8b541394f968ceb4f8bb2a4b10d7684a
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-14 11:49:39 +0100

    prepare shared_context for request specs #FL-398 comment

--- spec/requests/critical_path_autobuy_for_category_suppliers_spec.rb
+++ spec/requests/critical_path_autobuy_for_category_suppliers_spec.rb
@@ -1,23 +1,7 @@
 require 'spec_helper'
 
 describe 'Critical Path Autobuy for category suppliers' do
-  before do
-    Settings.big_supplier_purchase_limit = 10000
-    with_site('fairleads')
-    Locale.make!
-    VatRate.make!
-    without_confirmation_email!
-    without_invoice_email_template!
-    without_lead_notification!
-    admin.roles << :superadmin
-    admin.save!
-  end
-
-  let(:admin) { User::Admin.make!(:confirmed_at => Time.now) }
-  let(:seller) { Seller.make! }
-  let(:company_name) { 'CompanyName' }
-  let(:lead_category) { LeadCategory.find_by_name(company_name) }
-  let!(:currency) { Currency.make!(:name => 'EUR', :global_default => true) }
+  include_context 'request specs context'
 
   it 'autobuy for category suppliers' do
     # https://github.com/Selleo/NewBizzShoppen/wiki/Critical-Path---Autobuy-for-category-suppliers#wiki-subscription-creation
