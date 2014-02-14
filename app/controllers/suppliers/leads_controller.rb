001295721dbc2ea464fb6d4da498c4051ce05040
irek <irek.skrobis@gmail.com>
2014-02-14 14:15:07 +0100

    adding lead by supplier: view & controller, extend shared/leads/_form and break lines in this file #FL-354 comment

--- app/controllers/suppliers/leads_controller.rb
+++ app/controllers/suppliers/leads_controller.rb
@@ -1,6 +1,21 @@
 class Suppliers::LeadsController < Suppliers::BasicSupplierController
   before_filter :check_supplier_role
 
+  def new
+    @lead = Lead.new
+  end
+
+  def create
+    lead = Lead.new(params[:lead])
+    supplier_lead_adder = SupplierLeadAdder.new(lead, current_user)
+    if supplier_lead_adder.process
+      redirect_to suppliers_lead_purchases_path, :notice => I18n.t('lead_supplier.leads.create.controller.successful_created_notice')
+    else
+      @lead = supplier_lead_adder.lead
+      render :new
+    end
+  end
+
   private
 
   def check_supplier_role
