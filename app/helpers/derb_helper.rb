9bbe0948b94fc1a20e30cfd1991fbe5dc5b2535b
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-20 09:16:51 +0100

    ibt helper replace ibt tag by ibt helper  # FL-426 comment

---
+++ app/helpers/derb_helper.rb
@@ -0,0 +1,14 @@
+module DerbHelper
+  def ibt(opts = {}, &block)
+    classes = %w(bt)
+    classes << 'bt_icon' if opts[:icon]
+    classes << opts[:class] if opts[:class]
+    content_tag(:div, :class => classes) do
+      content_tag(:span) do
+        capture(&block).tap do |html|
+          html << image_tag("bt_icons/#{opts[:icon]}.png") if opts[:icon]
+        end
+      end
+    end
+  end
+end
