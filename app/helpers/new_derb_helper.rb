1fd8b745fe1d904ffead118a9ca69cc690ca14f1
Bartłomiej Danek <bartek.danek@gmail.com>
2014-02-23 12:58:12 +0100

    replace header tag by helper  FOR REVIEWERS:  - tag <header> is replaced by header helper  - helper header has to be put in block with `=`  - all data inside `section` block has to include `=`  - attribute mappers: c => class, other attribute without changes, config is passed as hash  - if tag includes back, header helper has to be called with the same options
     #FL-429  comment

--- app/helpers/new_derb_helper.rb
+++ app/helpers/new_derb_helper.rb
@@ -11,4 +11,41 @@ module NewDerbHelper
       end
     end
   end
+
+  def header(opts = {}, &block)
+    classes = %w(header_ribbon)
+    classes << opts[:class] if opts[:class]
+    back = opts[:back]
+    refresh = opts[:refresh]
+    content = content_tag(:div, :class => classes) do
+      content_tag(:h1) do
+        content_tag(:span) do
+          capture_section(:block_header, &block)
+        end
+      end +
+
+      unless back.blank?
+        backurl = opts[:backurl]
+
+        content_tag(:ul, :class => 'header_actions') do
+          content_tag(:li, :class => 'bt_back') do
+            if backurl
+              bt_link_to(nil, 'Back', backurl, :class => 'bt_grey_small')
+            else
+              bt_link_to(nil, 'Back', 'javascript:void(0)', :onclick => 'history.go(-1)', :class => 'bt_grey_small')
+            end
+          end
+        end
+      end +
+
+      unless refresh.blank?
+        content_tag(:ul, :class => 'header_actions') do
+          content_tag(:li, :class => 'bt_refresh') do
+            bt_link_to(nil, 'Refresh', 'javascript:location.reload(true)', :class => 'bt_grey_small')
+          end
+        end
+      end +
+      capture_section(:block_actions, &block)
+    end
+  end
 end
