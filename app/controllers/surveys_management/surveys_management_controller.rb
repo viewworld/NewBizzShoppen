080eda27585a4a0656670b24e486effea0683347
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-18 15:04:15 +0200

    fix current_user in survey_managements_controller

--- app/controllers/surveys_management/surveys_management_controller.rb
+++ app/controllers/surveys_management/surveys_management_controller.rb
@@ -1,19 +1,16 @@
 class SurveysManagement::SurveysManagementController < SecuredController
   def current_user
-    if user_signed_in?
-      @user ||= ::User::Admin.find_by_id(super.id)
-      @user ||= ::User::CallCentre.find_by_id(super.id)
-      @user ||= ::User::CategorySupplier.find_by_id(super.id)
-      @user ||= ::User::Supplier.find_by_id(super.id)
+    user = super
+
+    if user
+      @user ||= ::User::Admin.find(user.id)
+      @user ||= ::User::CallCentre.find(user.id)
+      @user ||= ::User::CategorySupplier.find(user.id)
+      @user ||= ::User::Supplier.find(user.id)
     end
   end
 
   private
-
-  def authorize_user_for_namespace!
-    authorize_role(:admin, :call_centre, :category_supplier, :supplier)
-    raise CanCan::AccessDenied unless current_user.surveys_enabled?
-  end
 
   def fetch_survey
     @survey = Survey.find(params[:survey_id])
