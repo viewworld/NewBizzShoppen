ae4126c3faadde8009a614152adce0acba442a2b
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-26 14:07:48 +0200

    refactor supplier_home controller a bit

--- app/controllers/supplier_home_controller.rb
+++ app/controllers/supplier_home_controller.rb
@@ -1,32 +1,45 @@
 class SupplierHomeController < ApplicationController
-
   set_tab "home"
-
-  private
-
-  def sales_manager
-    @latest_leads = Lead.without_inactive.interesting_for_user(current_user).published_only.without_bought_and_requested_by(current_user).with_supplier_unique_categories(current_user.id).latest.limit(3)
-    @my_leads     = current_user.has_role?(:supplier) ? current_user.bought_leads.latest.limit(3) : current_user.assigned_leads.latest.limit(3)
-    render :sales_manager
-  end
-
-  def guest
-    #@best_sellers = Lead.without_inactive.published_only.without_bought_and_requested_by(current_user).without_unique_categories.bestsellers.limit(3)
-    @latest_deals = Deal.without_inactive.published_only.latest.limit(3)
-    @latest_leads = Lead.without_inactive.published_only.without_bought_and_requested_by(current_user).without_unique_categories.latest.limit(3)
-    render :guest
-  end
-
-  public
 
   def show
     @news = Article::News::Supplier.published.latest.limit(3)
 
-    if user_signed_in? and current_user.has_role? :lead_supplier
-      sales_manager
+    if user_signed_in? && current_user.has_role? :lead_supplier
+      supplier
     else
       guest
     end
   end
 
+  private
+
+  def supplier
+    @latest_leads = Lead.without_inactive
+                        .interesting_for_user(current_user)
+                        .published_only
+                        .without_bought_and_requested_by(current_user)
+                        .with_supplier_unique_categories(current_user.id)
+                        .latest
+                        .limit(3)
+
+    @my_leads = current_user.has_role?(:supplier) ? current_user.bought_leads.latest.limit(3) : current_user.assigned_leads.latest.limit(3)
+
+    render :sales_manager
+  end
+
+  def guest
+    @latest_deals = Deal.without_inactive
+                        .published_only
+                        .latest
+                        .limit(3)
+
+    @latest_leads = Lead.without_inactive
+                        .published_only
+                        .without_bought_and_requested_by(current_user)
+                        .without_unique_categories
+                        .latest
+                        .limit(3)
+
+    render :guest
+  end
 end
