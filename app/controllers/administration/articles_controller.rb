7f3fef75d7d8f67e9509906bcd9e9db779122a6a
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-26 16:22:35 +0200

    refactor administration articles

--- app/controllers/administration/articles_controller.rb
+++ app/controllers/administration/articles_controller.rb
@@ -1,51 +1,46 @@
 class Administration::ArticlesController < Administration::AdministrationController
-  inherit_resources
-
   set_tab "content"
   set_subtab "articles"
 
-  before_filter :set_referer, :only => [:edit]
+  before_filter :set_referer, only: :edit
+  before_filter :set_article, only: [:show, :edit, :update, :destroy]
 
-  def edit
-    @article = Article.find(params[:id])
-    edit!
-  end
+  def index
+    params[:search] ||= {}
+    params[:search][:with_subclass] ||= "Article::Cms"
+    params[:search][:without_subclass] = "Article::Cms::Hint"
 
-  def update
-    @article = Article.find(params[:id])
-    @article.attributes = params[:article]
-    update! do |success, failure|
-      success.html {
-      if session[:articles_referer]
-        redirect_to session[:articles_referer]
-      else
-        redirect_to administration_articles_path
-      end
-      }
-      failure.html { render :action => :edit }
-    end
+    @search = Article.scoped_search(params[:search])
+    @articles = @search.paginate(show_all: params[:show_all], page: params[:page])
   end
 
   def create
-    params[:article_cms] ||= {}
     @article = Article::Cms::MainPageArticle.new(params[:article])
-    create! do |success, failure|
-      success.html { render :action => :edit }
-      failure.html { redirect_to administration_articles_path }
+
+    if @article.save
+      redirect_to edit_administration_article_path(@article)
+    else
+      redirect_to administration_articles_path
     end
   end
 
-  protected
+  def update
+    if @article.update_attributes(params[:article])
+      redirect_to session[:articles_referer] || administration_articles_path
+    else
+      render :edit
+    end
+  end
 
-  def collection
-    params[:search] ||= {}
-    params[:search][:with_subclass] = "Article::Cms" unless params[:search][:with_subclass].present?
-    params[:search][:without_subclass] = "Article::Cms::Hint"
-    @search = Article.scoped_search(params[:search])
-    @articles = @search.paginate(:show_all => params[:show_all], :page => params[:page])
+  def destroy
+    @article.destroy
+    redirect_to administration_articles_path
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def authorize_user_for_namespace!
     authorize_role(:translator)
