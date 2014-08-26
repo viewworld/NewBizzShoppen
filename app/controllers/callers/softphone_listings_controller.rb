ea430d466c85d6ea91f70ce521bb2477bf69a903
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-26 11:00:45 +0200

    fix User.with_any_role

--- app/controllers/callers/softphone_listings_controller.rb
+++ app/controllers/callers/softphone_listings_controller.rb
@@ -3,7 +3,7 @@ class Callers::SoftphoneListingsController < Callers::CallerController
   set_subtab 'softphone_listing'
 
   def show
-    @search = User.without_locked.with_any_role(:agent, :call_centre_agent).order("first_name").scoped_search(params[:search])
+    @search = User.without_locked.with_any_role([:agent, :call_centre_agent]).order("first_name").scoped_search(params[:search])
     @agents = @search.paginate(:page => params[:page], :per_page => 30)
   end
 end
