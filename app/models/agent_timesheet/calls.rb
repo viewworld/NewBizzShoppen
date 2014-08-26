c03e63e466e26e3589559caa0708f4b0dc0ad82d
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-26 11:44:12 +0200

    refactor agent_timesheet models

--- app/models/agent_timesheet/calls.rb
+++ app/models/agent_timesheet/calls.rb
@@ -1,7 +1,5 @@
 class AgentTimesheet::Calls < ActiveRecord::Base
-
   set_table_name 'agent_timesheets_calls'
 
   include AgentTimesheet::TimesheetBase
-
 end
--- app/models/agent_timesheet/cost.rb
+++ app/models/agent_timesheet/cost.rb
@@ -1,7 +1,5 @@
 class AgentTimesheet::Cost < ActiveRecord::Base
-
   set_table_name 'agent_timesheets_cost'
 
   include AgentTimesheet::TimesheetBase
-
 end
