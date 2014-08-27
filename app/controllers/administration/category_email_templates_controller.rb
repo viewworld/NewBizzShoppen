0a28159bae827377289ea61c9124c23a5f9fc0d9
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-27 12:13:15 +0200

    refactor administration category_email_templates

--- app/controllers/administration/category_email_templates_controller.rb
+++ app/controllers/administration/category_email_templates_controller.rb
@@ -1,29 +1,33 @@
 class Administration::CategoryEmailTemplatesController < Administration::AdministrationController
   set_tab "browse_leads"
 
-  before_filter :set_category, :only => [:edit, :update]
+  before_filter :set_category, only: [:edit, :update]
 
   def edit
-    global_et = EmailTemplate.global.where(:uniq_id => 'bought_lead_notification').first
-    @email_template = @category.email_template ? @category.email_template : @category.create_email_template(:subject => global_et.subject, :body => global_et.body, :from => global_et.from, :uniq_id => global_et.uniq_id)
+    @email_template = @category.email_template || duplicate_global_email_template
   end
 
   def update
-    @email_template = @category.email_template
-    @email_template.update_attributes(params[:email_template])
-    if @email_template.save
-      flash[:notice] = t('administration.category_email_templates.update.controller.successful_update_notice')
-      redirect_to categories_path
+    if @category.email_template.update_attributes(params[:email_template])
+      redirect_to categories_path, notice: t('administration.category_email_templates.update.controller.successful_update_notice')
     else
       render :edit
     end
   end
 
   private
-
   def set_category
     @category = LeadCategory.find(params[:id])
   end
 
+  def duplicate_global_email_template
+    global_template = EmailTemplate.global.where(uniq_id: 'bought_lead_notification').first
 
+    @category.create_email_template(
+      subject: global_template.subject,
+      body: global_template.body,
+      from: global_template.from,
+      uniq_id: global_template.uniq_id
+    )
+  end
 end
