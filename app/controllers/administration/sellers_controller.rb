fbadf04f95d6431ccb6c5cdc2b8650459a71b54a
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-27 15:06:43 +0200

    refactor admin sellers

--- app/controllers/administration/sellers_controller.rb
+++ app/controllers/administration/sellers_controller.rb
@@ -1,17 +1,44 @@
 class Administration::SellersController < Administration::AdministrationController
-  inherit_resources
-
   set_tab "sellers"
 
-  def destroy
-    super do |success,failure|
-      success.html do
-        redirect_to administration_sellers_path
-      end
-      failure.html do
-        redirect_to :back
-      end
+  before_filter :set_seller, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @sellers = Seller.all
+  end
+
+  def new
+    @seller = Seller.new
+  end
+
+  def create
+    @seller = Seller.new(params[:seller])
+
+    if @seller.save
+      redirect_to administration_sellers_path
+    else
+      render :new
     end
   end
 
+  def update
+    if @seller.update_attributes
+      redirect_to administration_sellers_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @seller.destroy
+      redirect_to administration_sellers_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def set_seller
+    @seller = Seller.find(params[:id])
+  end
 end
