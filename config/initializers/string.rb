b0041a8aa1b222c380980220b6c24921d2b7394d
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-28 11:23:17 +0200

    remove unnecessary self from string db_downcase method

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,6 +1,6 @@
 class String
   def db_downcase
-    result = ::ActiveRecord::Base.connection.execute "select lower(#{ActiveRecord::Base.sanitize(self.to_s)})"
+    result = ::ActiveRecord::Base.connection.execute "select lower(#{ActiveRecord::Base.sanitize(to_s)})"
     result.first["lower"]
   end
 end
