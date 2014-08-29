26bbf65d39638e000b9273d7db07cc5af869f6c0
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-08-29 15:32:50 +0200

    refactor admin softphone_servers

--- app/controllers/administration/softphone_servers_controller.rb
+++ app/controllers/administration/softphone_servers_controller.rb
@@ -1,34 +1,45 @@
 class Administration::SoftphoneServersController < Administration::AdministrationController
-  inherit_resources
-  actions :all, :except => [:show]
-
   set_tab "settings"
   set_subtab "softphone_servers"
 
-  public
+  before_filter :set_softphone_server, only: [:edit, :update, :destroy]
+
+  def index
+    @softphone_servers = SoftphoneServer.all
+  end
+
+  def new
+    @softphone_server = SoftphoneServer.new
+  end
 
   def create
-    create! do |success,failure|
-      success.html { redirect_to administration_softphone_servers_path }
-      failure.html { render :action => :new }
+    @softphone_server = SoftphoneServer.new(params[:softphone_server])
+    
+    if @softphone_server.save
+      redirect_to administration_softphone_servers_path
+    else
+      render :new
     end
   end
 
   def update
-    update! do |success,failure|
-      success.html { redirect_to administration_softphone_servers_path }
-      failure.html { render :action => :edit }
+    if @softphone_server.update_attributes(params[:softphone_server])
+      redirect_to administration_softphone_servers_path
+    else
+      render :edit
     end
   end
 
   def destroy
-    destroy! do |success,failure|
-      success.html { redirect_to administration_currencies_path }
-      failure.html {
-        flash[:alert] = @softphone_server.errors[:base].join('<br/>')
-        redirect_to administration_softphone_servers_path
-      }
+    if @softphone_server.destroy
+      redirect_to administration_softphone_servers_path
+    else
+      redirect_to administration_softphone_servers_path, alert: @softphone_server.errors[:base].join('<br/>')
     end
   end
 
+  private
+  def set_softphone_server
+    @softphone_server = SoftphoneServer.find(params[:id])
+  end
 end
