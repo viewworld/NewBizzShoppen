b1e76c2c38635d9a29dba3c0bf003f9a7e069bb6
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-01 14:02:06 +0200

    [skip review] refactor admin settings a bit

--- app/controllers/administration/settings_controller.rb
+++ app/controllers/administration/settings_controller.rb
@@ -1,25 +1,21 @@
 class Administration::SettingsController < Administration::AdministrationController
-
   set_tab "settings"
   set_subtab "global"
 
   def edit
     @settings = Settings.order("var ASC")
-    @bank_accounts_search = BankAccount.scoped_search(params[:bank_accounts])
-    @bank_accounts_search.ascend_by_id = true unless params[:bank_accounts]
-    @bank_accounts = @bank_accounts_search.all
+    @bank_accounts = BankAccount.order("id ASC")
     @vat_rates = VatRate.all
     @youtube_introductions = YoutubeIntroduction.all
   end
 
   def update
-    @settings = Settings.where({})
+    @settings = Settings.all
 
     params[:settings].each_pair do |id, value|
       @settings.detect { |s| s.id ==  id.to_i}.update_attribute(:value, value)
     end
 
-    flash[:notice] = I18n.t("administration.settings.update.controller.successful_update_notice")
-    redirect_to edit_administration_setting_path
+    redirect_to edit_administration_setting_path, notice: t("administration.settings.update.controller.successful_update_notice")
   end
 end
