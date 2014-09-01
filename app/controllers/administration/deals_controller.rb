057a58b277cfa76011f6c07c83d169563782872e
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-01 16:27:54 +0200

    [skip review] refactor admin deals

--- app/controllers/administration/deals_controller.rb
+++ app/controllers/administration/deals_controller.rb
@@ -1,13 +1,12 @@
 class Administration::DealsController < Administration::AdministrationController
-  before_filter :set_deal, :only => [:edit, :update, :destroy]
-  before_filter :prepare_assets, :only => [:edit, :update]
+  before_filter :set_deal, only: [:edit, :update, :destroy]
+  before_filter :prepare_assets, only: [:edit, :update]
   cache_sweeper :deal_sweeper
 
   include ::DealActions
   include ::DealCreateActions
 
   private
-
   def success(message)
     flash[:notice] = message
     success_redirect_to("administration")
