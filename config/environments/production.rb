523a99d7929b688ca3b92e43b35cb9aa71889efe
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-01 12:27:02 +0200

    disable digest for assets as sass-rails helper refuse to work with this 3.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Nbs::Application.configure do
   # Assets
   config.assets.compress = true
   config.assets.compile = false
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Specifies the header that your server uses for sending files
   #config.action_dispatch.x_sendfile_header = "X-Sendfile"
