c93b5a58e5fbe1934b4451e6b7ad5c57d77f9cc1
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-01 16:49:58 +0200

    refactor admin results

--- app/controllers/administration/results_controller.rb
+++ app/controllers/administration/results_controller.rb
@@ -1,21 +1,25 @@
 class Administration::ResultsController < Administration::AdministrationController
-  inherit_resources
-
   set_tab "settings"
   set_subtab "results"
 
+  before_filter :set_result, only: [:edit, :update]
+
+  def index
+    @search = Result.scoped_search(params[:search])
+    @search.with_archived ||= 0
+    @results = @search.order("name").paginate(show_all: params[:show_all], page: params[:page], per_page: Result.per_page)
+  end
+
   def update
-    update! do |success, failure|
-      success.html { redirect_to administration_results_path }
+    if @result.update_attributes(params[:result_params])
+      redirect_to administration_results_path
+    else
+      render :edit
     end
   end
 
-  protected
-
-  def collection
-    @search = Result.scoped_search(params[:search])
-    @search.with_archived ||= 0
-    @results = @search.order("name").paginate(:show_all => params[:show_all], :page => params[:page], :per_page => Result.per_page)
+  private
+  def set_result
+    @result = Result.find(params[:id])
   end
-
 end
