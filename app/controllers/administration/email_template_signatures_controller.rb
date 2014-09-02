4c618095e8877bf07f9c194ce5aef0b592c86db5
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-02 13:02:13 +0200

    refactor admin email_template_signatures

--- app/controllers/administration/email_template_signatures_controller.rb
+++ app/controllers/administration/email_template_signatures_controller.rb
@@ -1,24 +1,47 @@
 class Administration::EmailTemplateSignaturesController < Administration::AdministrationController
-  inherit_resources
-
   set_tab "settings"
   set_subtab "email_template_signatures"
 
+  before_filter :set_email_collection, only: [:edit, :new]
+  before_filter :set_email_template_signature, only: [:edit, :update, :destroy]
+
+  def index
+    @email_template_signatures = EmailTemplateSignature.custom.paginate(show_all: params[:show_all], page: params[:page])
+  end
+
+  def new
+    @email_template_signature = EmailTemplateSignature.new
+  end
+
   def create
-    create! do |success, failure|
-      success.html { redirect_to administration_email_template_signatures_path }
+    @email_template_signature = EmailTemplateSignature.new(params[:email_template_signature])
+
+    if @email_template_signature.save
+      redirect_to administration_email_template_signatures_path
+    else
+      render :new
     end
   end
 
   def update
-    update! do |success, failure|
-      success.html { redirect_to administration_email_template_signatures_path }
+    if @email_template_signature.update_attributes(params[:email_template_signature])
+      redirect_to administration_email_template_signatures_path
+    else
+      render :new
     end
   end
 
-  protected
+  def destroy
+    @email_template_signature.destroy
+    redirect_to administration_email_template_signatures_path
+  end
 
-  def collection
-    @email_template_signatures ||= end_of_association_chain.custom.paginate(:show_all => params[:show_all], :page => params[:page], :per_page => EmailTemplateSignature.per_page)
+  private
+  def set_email_collection
+    @emails = EmailTemplateSignature::FROM_EMAILS.map { |e| [e, e] }
+  end
+
+  def set_email_template_signature
+    @email_template_signature = EmailTemplateSignature.find(params[:id])
   end
 end
