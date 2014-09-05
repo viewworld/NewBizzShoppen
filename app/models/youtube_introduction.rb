f1a4e570ad7c5cc1b927ffdd35e7c40724940d76
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-05 15:23:09 +0200

    refactor youtube_introduction model

--- app/models/youtube_introduction.rb
+++ app/models/youtube_introduction.rb
@@ -1,19 +1,9 @@
 class YoutubeIntroduction < ActiveRecord::Base
-
-  has_one :image,
-          :class_name => "Asset::YoutubeImage",
-          :as         => :assetable,
-          :conditions => "asset_type = 'Asset::YoutubeImage'",
-          :dependent  => :destroy
-
-  SITES = ['fairleads', 'fairdeals']
-
-  validates_presence_of :url, :locale_code, :site
-
-  validates_uniqueness_of :locale_code, :scope => :site
-
-  scope :for_site, lambda{|site| where(:site => site)}
-
+  has_one :image, as: :assetable, class_name: 'Asset::YoutubeImage', dependent: :destroy
   accepts_nested_attributes_for :image
 
+  validates_presence_of :url, :locale_code, :site
+  validates_uniqueness_of :locale_code, scope: :site
+
+  scope :for_site, ->(site) { where(site: site) }
 end
