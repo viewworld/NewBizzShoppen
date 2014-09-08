69733a33a5b3d9980384a342d9eec720945545f4
Bartłomiej Danek <bartek.danek@gmail.com>
2014-09-08 14:04:01 +0200

    rewrite Rack::LocaleSetter middlerware

--- lib/locale_setter.rb
+++ lib/locale_setter.rb
@@ -1,26 +1,35 @@
 module Rack
   class LocaleSetter
-    def initialize app
+    def initialize(app)
       @app = app
     end
+
+    def call(env)
+      I18n.locale = env['rack.session'][:locale_code] = translate_locale(find_locale(env))
+      @app.call env
+    end
+
+    private
 
     def locales
       @locales ||= Locale.all.map(&:code)
     end
 
     def translate_locale(locale)
-      dictionary = {'eu' => 'en', 'com' => 'en', 'dk' => 'da'}
-      locale = dictionary[locale.to_s] || locale.to_s
+      locale = dictionary[locale] || locale
       locales.include?(locale) ? locale : 'en'
     end
 
     def find_locale(env)
-       env['rack.session'][:locale_code] || env["HTTP_HOST"].to_s[/fairdeals\.(\w{2,4})/,1] || env["HTTP_HOST"].to_s[/faircalls\.(\w{2,4})/,1] || env["HTTP_ACCEPT_LANGUAGE"].to_s[/^([a-z]{2})/]
+      @env_locale ||=
+        (env['rack.session'][:locale_code] ||
+         env["HTTP_HOST"].to_s[/fairdeals\.(\w{2,4})/,1] ||
+         env["HTTP_HOST"].to_s[/faircalls\.(\w{2,4})/,1] ||
+         env["HTTP_ACCEPT_LANGUAGE"].to_s[/^([a-z]{2})/]).to_s
     end
 
-    def call(env)
-      I18n.locale = env['rack.session'][:locale_code] = translate_locale(find_locale(env))
-      @app.call env
+    def dictionary
+      {'eu' => 'en', 'com' => 'en', 'dk' => 'da'}
     end
   end
 end
