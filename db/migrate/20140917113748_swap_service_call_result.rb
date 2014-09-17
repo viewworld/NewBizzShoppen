6b7b85ccf8c47e81fca01568e8087ec012271afc
Bartłomiej Danek <bartek.danek@gmail.com>
2014-09-17 14:29:38 +0200

    fix migration

--- db/migrate/20140917113748_swap_service_call_result.rb
+++ db/migrate/20140917113748_swap_service_call_result.rb
@@ -3,13 +3,16 @@ class SwapServiceCallResult < ActiveRecord::Migration
     result = Result.create(:name => 'Service call', :generic => true)
     result.result_fields.create(:name => 'Service call date', :field_type => 4)
 
-    et = EmailTemplate.where(:uniq_id => 'upgrade_contact_to_member', :resource_id => nil).first.clone
-    et.uniq_id = 'service_call'
-    et.name = 'Upgrade contact to member through Service call'
-    et.subject = 'You have been upgraded to member through Service call'
-    et.save
+    et_origin = EmailTemplate.where(:uniq_id => 'upgrade_contact_to_member', :resource_id => nil).first
+    if et_origin
+      et = et_origin.clone
+      et.uniq_id = 'service_call'
+      et.name = 'Upgrade contact to member through Service call'
+      et.subject = 'You have been upgraded to member through Service call'
+      et.save
 
-    Campaign.all.each(&:save)
+      Campaign.all.each(&:save)
+    end
   end
 
   def self.down
