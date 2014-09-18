ac971cf6106e55d9d5b20d1f2547c1519b20e94c
Bartłomiej Danek <bartek.danek@gmail.com>
2014-09-18 13:04:56 +0200

    rewrite schedule.rb #FL-503 comment

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,8 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+job_type :thor, "cd :path && :environment_variable=:environment bundle exec thor nbs::task :output"
+
 if self.environment == 'production'
   dir_pwd = Dir.pwd.gsub(/(releases\/\d+)/, 'current')
 
@@ -11,39 +13,40 @@ if self.environment == 'production'
   hours = %w{1 1 2 2 3 3 5}
   ['recalculate_leads_average_ratings', 'refresh_subaccounts_counters', 'refresh_agent_counters', 'check_lead_certification_requests'].each_with_index do |thor_task, i|
     every 1.day, :at => "#{hours[i]}:#{i % 2 == 0 ? '00' : '30' } am", :roles => [:app] do
-      command File.join(dir_pwd, "RAILS_ENV=production thor nbs:#{thor_task}")
+      thor :task => thor_task
     end
   end
 
   every 1.day, :at => '4:00 am' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:refresh_exchange_rates')
+    thor :task => :refresh_exchange_rates
   end
 
   every 1.day, :at => '8:00 pm' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:send_daily_lead_notifications')
+    thor :task => :send_daily_lead_notifications
   end
 
   every :sunday, :at => '8:00 pm' do
     command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:send_daily_lead_notifications')
+    thor :task => :send_daily_lead_notifications
   end
 
   every 1.day, :at => '0:00' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:prolong_subscriptions_daily')
+    thor :task => :prolong_subscriptions_daily
   end
 
   every 1.day, :at => '1:00 am' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:create_unpaid_invoices_for_unpaid_sub_periods')
+    thor :task => :create_unpaid_invoices_for_unpaid_sub_periods
   end
 
   every 8.hours do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:synchronize_newsletter_lists')
+    thor :task => :synchronize_newsletter_lists
   end
 
   every 1.day, :at => '6:00' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:merge_regular_user_session_logs')
+    thor :task => :merge_regular_user_session_logs
   end
 
   every 1.day, :at => '2:00 am' do
-    command File.join(dir_pwd, 'RAILS_ENV=production thor nbs:import_contacts_from_newsletter_lists')
+    thor :task => :import_contacts_from_newsletter_lists
   end
 end
