74cfb79ba981a1b32ac723b36145796a8afbab40
Bartłomiej Danek <bartek.danek@gmail.com>
2014-09-24 13:40:58 +0200

    shared softphone panel for administrator - rename inherited_resource intance name #FL-544 comment

--- app/controllers/administration/shared_softphones_controller.rb
+++ app/controllers/administration/shared_softphones_controller.rb
@@ -1,6 +1,6 @@
 class Administration::SharedSoftphonesController < Administration::AdministrationController
   inherit_resources
-  defaults :resource_class => Softphone
+  defaults :resource_class => Softphone, :instance_name => :softphone
   actions :all, :except => [:show]
 
   set_tab 'settings'
@@ -8,7 +8,7 @@ class Administration::SharedSoftphonesController < Administration::Administratio
 
   def create
     create! do |success,failure|
-      success.html { redirect_to administration_softphone_servers_path }
+      success.html { redirect_to administration_shared_softphones_path }
       failure.html { render :new }
     end
   end
@@ -17,16 +17,6 @@ class Administration::SharedSoftphonesController < Administration::Administratio
     update! do |success,failure|
       success.html { redirect_to administration_shared_softphones_path }
       failure.html { render :edit }
-    end
-  end
-
-  def destroy
-    destroy! do |success,failure|
-      success.html { redirect_to administration_shared_softphones_path }
-      failure.html do
-        flash[:alert] = @softphone_server.errors[:base].to_sentence
-        redirect_to administration_softphone_servers_path
-      end
     end
   end
 end
