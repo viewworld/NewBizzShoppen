dcd4040f763db563cae40cc5b8318e11c5973aa3
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-24 13:28:48 +0200

    refactor admin bulk users update part 2

--- app/controllers/administration/bulk_users_update_controller.rb
+++ app/controllers/administration/bulk_users_update_controller.rb
@@ -3,15 +3,7 @@ class Administration::BulkUsersUpdateController < Administration::Administration
     @users = User::Abstract.where(id: params[:user_ids])
 
     if params[:bulk_action_type] == 'create_invoice'
-      ActiveRecord::Base.transaction do
-        @users.each do |user|
-          if user.big_buyer?
-            user.lead_purchases.each do |lead_purchase|
-              Invoice.create(user_id: user.id, currency_id: lead_purchase.currency_id)
-            end
-          end
-        end
-      end
+      bulk_create_invoices_for(@users.select(&:big_buyer?))
     elsif params[:bulk_action_type] == 'unlock'
       @users.update_all(locked_at: nil)
     elsif params[:bulk_action_type] == 'lock'
@@ -19,5 +11,16 @@ class Administration::BulkUsersUpdateController < Administration::Administration
     end
 
     redirect_to administration_users_path, notice: t('flash.bulk_users_update.update.notice')
+  end
+
+  private
+  def bulk_create_invoices_for(users)
+    ActiveRecord::Base.transaction do
+      users.each do |user|
+        user.lead_purchases.each do |lead_purchase|
+          Invoice.create(user_id: user.id, currency_id: lead_purchase.currency_id)
+        end
+      end
+    end
   end
 end
