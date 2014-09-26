e1e4a5d29ebd314e27d32fb24c55b0e3f9a20fda
Bartłomiej Danek <bartek.danek@gmail.com>
2014-09-26 10:12:22 +0200

    shared softphone number - fix validation #FL-544 comment

--- app/models/softphone.rb
+++ app/models/softphone.rb
@@ -6,11 +6,17 @@ class Softphone < ActiveRecord::Base
   scope :shared, where(:user_id => nil)
   scope :available, lambda { |user| where('user_id IS NULL OR user_id = ?', user) }
 
-  validates :name, :presence => true,
-    :if => lambda { self.is_a?(Softphone) ? [sip_username, sip_password, softphone_server_id].any?(&:present?) : true }
+  validates :name, :presence => true, :if => lambda { self.is_a?(Softphone) ? validates_name? : true }
   validates :name, :uniqueness => { :scope => :user_id }
 
   def copy_attributes
     attributes.reject {|attr,_| !%w(sip_username sip_password softphone_server_id).include?(attr)}
+  end
+
+  private
+
+  def validates_name?
+    fields = [sip_username, sip_password, softphone_server_id]
+    fields.any?(&:present?) || fields.all?(&:blank?)
   end
 end
