e7a24f0ddc7bb4364f4e3b8ca828e4baff300b69
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-30 11:48:28 +0200

    refactor admin ivoicing invoices

--- app/controllers/administration/invoicing/invoices_controller.rb
+++ app/controllers/administration/invoicing/invoices_controller.rb
@@ -1,39 +1,57 @@
 class Administration::Invoicing::InvoicesController < Administration::SuperadminController
-  inherit_resources
-#  defaults :redirects => {:create => :edit, :update => :show}
-#  set_search_persistence :name => "invoices_filter"
-  set_tab "reports"
-  set_subtab "invoices"
+  set_tab 'reports'
+  set_subtab 'invoices'
 
-  def create
-    @invoice = Invoice.new(params[:invoice])
-    create! do |success, failure|
-      success.html { redirect_to edit_administration_invoicing_invoice_path(@invoice) }
-    end
-  end
+  before_filter :set_invoice, only: [:show, :edit, :update, :destroy]
 
-  def edit
-    super do |format|
-      format.html {@invoice.invoice_lines.build if @invoice.invoice_lines.blank? }
-    end
-  end
-
-  def show
-    super do |format|
-      format.html
-      format.pdf { send_file @invoice.store_pdf(current_user), :type => 'application/pdf'}
-    end
-  end
-
-  protected
-
-  def collection
+  def index
     @search = Invoice.scoped_search(params[:search])
     @search.descend_by_sale_date_and_number = true unless params[:search]
-    @invoices = @search.paginate :page => params[:page], :per_page => 20
+    @invoices = @search.paginate(page: params[:page], per_page: 20)
+
     @total_invoices = Invoice.count
     @total_paid_invoices = Invoice.total_paid.size
     @total_unpaid_invoices = Invoice.total_not_paid.size
   end
 
+  def show
+    respond_to do |format|
+      format.html
+      format.pdf { send_file @invoice.store_pdf(current_user), type: 'application/pdf' }
+    end
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to edit_administration_invoicing_invoice_path(@invoice)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @invoice.update_attributes(invoice_params)
+      @invoice.invoice_lines.build if @invoice.invoice_lines.blank?
+      redirect_to administration_invoicing_invoice_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @invoice.destroy
+    redirect_to administration_invoicing_invoice_path
+  end
+
+  private
+
+  def set_invoice
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def invoice_params
+    params[:invoice]
+  end
 end
