ad47cfb73a76701dc8e6d01eb9562c05ca9d8772
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-09-30 13:12:03 +0200

    refactor admin invoicing upcoming_invoices

--- app/controllers/administration/invoicing/upcoming_invoices_controller.rb
+++ app/controllers/administration/invoicing/upcoming_invoices_controller.rb
@@ -1,18 +1,13 @@
 class Administration::Invoicing::UpcomingInvoicesController < Administration::SuperadminController
-  inherit_resources
+  set_tab 'reports'
+  set_subtab 'upcoming_invoices'
 
-  set_tab "reports"
-  set_subtab "upcoming_invoices"
-
-  protected
-
-  def collection
+  def index
     params[:search] ||= {}
-    params[:search][:with_not_invoiced] = "1"
+    params[:search][:with_not_invoiced] = '1'
     @search = Debtor.scoped_search(params[:search])
-    @lead_purchases = @search.order("currency_id ASC").all
+    @lead_purchases = @search.order('currency_id ASC').all
     @total_euro_sum = @lead_purchases.inject(0.0){ |result, lead_purchase| lead_purchase.not_invoiced_euro_sum.to_f + result}
     @lead_purchases_count = @lead_purchases.count
   end
-
 end
