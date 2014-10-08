85366a337f559453a5e6b0b7a805f2eaf3110818
Bartłomiej Danek <bartek.danek@gmail.com>
2014-10-08 15:48:03 +0200

    run sign up process inside transaction #FL-570 comment

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -14,54 +14,55 @@ class SignInController < ApplicationController
   end
 
   def create(user_class_name, param_key, success_notice, path = root_path)
-    data = params[:token] ? RPXNow.user_data(params[:token], :raw_response => true)['profile'] : nil
-    data = session[:rpx_data] if data.blank?
-    user = User.find_by_email(data['verifiedEmail']) unless data.blank?
-    if user
-      user.update_attribute(:rpx_identifier, data['identifier'])
-      flash[:notice] = t("devise.sessions.new.controller.successfully_connect_to_social", :first_name => user.first_name, :account_type => User.social_provider(user.rpx_identifier), :email => user.email)
-      sign_in(user)
-      if user.has_role? :category_supplier and user.sign_in_count == 1
-        return redirect_to my_profile_path
-      else
-        return redirect_to root_path
+    ActiveRecord::Base.transaction do
+      data = params[:token] ? RPXNow.user_data(params[:token], :raw_response => true)['profile'] : nil
+      data = session[:rpx_data] if data.blank?
+      user = User.find_by_email(data['verifiedEmail']) unless data.blank?
+      if user
+        user.update_attribute(:rpx_identifier, data['identifier'])
+        flash[:notice] = t("devise.sessions.new.controller.successfully_connect_to_social", :first_name => user.first_name, :account_type => User.social_provider(user.rpx_identifier), :email => user.email)
+        sign_in(user)
+        if user.has_role? :category_supplier and user.sign_in_count == 1
+          return redirect_to my_profile_path
+        else
+          return redirect_to root_path
+        end
       end
-    end
-    @user = user_class_name.new(params[param_key])
-    @user.set_fields_for_rpx(data) unless data.blank?
-    if request.referer.to_s.include?("certification_accounts")
-      @user.skip_email_verification = "1"
-      @user.assign_free_subscription_plan = true
-    end
+      @user = user_class_name.new(params[param_key])
+      @user.set_fields_for_rpx(data) unless data.blank?
+      if request.referer.to_s.include?("certification_accounts")
+        @user.skip_email_verification = "1"
+        @user.assign_free_subscription_plan = true
+      end
 
-    if @deal = Deal.find_by_id(params[:deal_request_id])
-      @lead = Lead.new
-      @lead.based_on_deal(@deal, @user)
-    end
+      if @deal = Deal.find_by_id(params[:deal_request_id])
+        @lead = Lead.new
+        @lead.based_on_deal(@deal, @user)
+      end
 
-    respond_to do |format|
-      if @user.save
-        success_notice = I18n.t("flash.accounts.create.no_verification") if @user.confirmed?
+      respond_to do |format|
+        if @user.save
+          success_notice = I18n.t("flash.accounts.create.no_verification") if @user.confirmed?
 
-        if ["fairdeals", "fairleads"].include?(session[:site]) and @user.has_any_role?(:member, :category_supplier, :supplier) and @user.confirmed? and @user.rpx_identifier.blank?
-          @user.send_invitation_email(params[param_key][:password]) if @user.member?
-          path = session[:user_return_to] if session[:user_return_to]
-          success_notice = I18n.t("devise.sessions.new.controller.successfully_logged_in")
-          sign_in(@user)
+          if ["fairdeals", "fairleads"].include?(session[:site]) and @user.has_any_role?(:member, :category_supplier, :supplier) and @user.confirmed? and @user.rpx_identifier.blank?
+            @user.send_invitation_email(params[param_key][:password]) if @user.member?
+            path = session[:user_return_to] if session[:user_return_to]
+            success_notice = I18n.t("devise.sessions.new.controller.successfully_logged_in")
+            sign_in(@user)
+          end
+          unless @user.rpx_identifier.blank?
+            @user.confirm!
+            sign_in(@user)
+          end
+
+          flash[:notice] = @user.rpx_identifier.blank? ? success_notice : I18n.t("devise.sessions.new.controller.successfully_logged_in")
+          format.html { redirect_to(path) }
+          format.js {  }
+        else
+          format.html { render("new") }
+          format.js {  }
         end
-        unless @user.rpx_identifier.blank?
-          @user.confirm!
-          sign_in(@user)
-        end
-
-        flash[:notice] = @user.rpx_identifier.blank? ? success_notice : I18n.t("devise.sessions.new.controller.successfully_logged_in")
-        format.html { redirect_to(path) }
-        format.js {  }
-      else
-        format.html { render("new") }
-        format.js {  }
       end
     end
   end
-
 end
