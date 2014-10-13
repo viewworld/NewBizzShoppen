14b4f0b9b60f2889253e82c3672a52f95706a42a
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-10-13 16:59:24 +0200

    refactor a bit models material

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,10 +1,14 @@
 class Material < Asset
-  belongs_to :campaign, :polymorphic => true
   has_attached_file :data, attachment_options
-  validates_attachment_size :data, :less_than => 10.megabytes
-  validates_attachment_content_type :data, :content_type => Asset::IMAGE_FILE_TYPES + Asset::DOCUMENT_FILE_TYPES, :message => " - #{I18n.t(:validation_asset_images_type)}"
+
+  validates_attachment_size :data, less_than: 10.megabytes
+  validates_attachment_content_type :data,
+    content_type: Asset::IMAGE_FILE_TYPES + Asset::DOCUMENT_FILE_TYPES
+
+  belongs_to :campaign, polymorphic: true
   has_many :result_values,
-           :finder_sql => 'SELECT result_values.* FROM result_values WHERE (field_type = \'#{ResultField::MATERIAL}\' AND value = \'#{id}\')'
+    finder_sql: 'SELECT result_values.* FROM result_values WHERE (field_type = \'#{ResultField::MATERIAL}\' AND value = \'#{id}\')'
+
   before_destroy do
     if result_values.any?
       self.errors.add(:base, I18n.t("materials.views.index.assigned_to_results"))
@@ -12,21 +16,7 @@ class Material < Asset
     end
   end
 
-  protected
-
-  def destroy_attached_files
-    if result_values.any?
-      self.errors.add(:base, I18n.t("materials.views.index.assigned_to_results"))
-      false
-    else
-      super
-    end
-  end
-
-  public
-
-  def url(style=nil, use_timestamp = false)
+  def url(style = nil, use_timestamp = false)
     super
   end
-
 end
