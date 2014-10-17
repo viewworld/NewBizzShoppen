2651849cfd2ca88a11e6a852f032306887f44bef
Bartłomiej Danek <bartek.danek@gmail.com>
2014-10-17 14:16:53 +0200

    display deal selector FL-584 comment

--- app/models/call_result.rb
+++ app/models/call_result.rb
@@ -84,7 +84,7 @@ class CallResult < ActiveRecord::Base
   scope :empty, where("1=0")
   default_scope :order => 'call_results.created_at DESC'
 
-  delegate :create_deals?, :to => :campaign_result, :allow_nil => true, :prefix => :campaign
+  delegate :create_deals?, :to => :campaign_result, :allow_nil => true
 
   amoeba do
     enable
@@ -100,11 +100,11 @@ class CallResult < ActiveRecord::Base
   end
 
   def upgrades_to_member?
-    result.upgrades_to_member? || campaign_create_deals?
+    result.upgrades_to_member? || create_deals?
   end
 
   def campaign_create_deals?
-    campaign_result.create_deals? && User.find_by_email(contact.email_address).nil?
+    create_deals? && User.find_by_email(contact.email_address).nil?
   end
 
   def campaign_result
