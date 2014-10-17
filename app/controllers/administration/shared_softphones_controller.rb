40d3ffe9c1385cb98cd9b1da3b7ce3a240f380cf
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-10-17 11:52:53 +0200

    refactor admin shared softphones

--- app/controllers/administration/shared_softphones_controller.rb
+++ app/controllers/administration/shared_softphones_controller.rb
@@ -1,28 +1,47 @@
 class Administration::SharedSoftphonesController < Administration::AdministrationController
-  inherit_resources
-  defaults :resource_class => Softphone, :instance_name => :softphone
-  actions :all, :except => [:show]
-
   set_tab 'settings'
   set_subtab 'shared_softphones'
 
+  before_filter :set_softphone, only: [:edit, :update, :destroy]
+
+  def index
+    @shared_softphones ||= Softphone.shared
+  end
+
+  def new
+    @softphone = Softphone.new
+  end
+
   def create
-    create! do |success,failure|
-      success.html { redirect_to administration_shared_softphones_path }
-      failure.html { render :new }
+    @softphone = Softphone.new(softphone_params)
+
+    if @softphone.save
+      redirect_to administration_shared_softphones_path
+    else
+      render :new
     end
   end
 
   def update
-    update! do |success,failure|
-      success.html { redirect_to administration_shared_softphones_path }
-      failure.html { render :edit }
+    if @softphone.update_attributes(softphone_params)
+      redirect_to administration_shared_softphones_path
+    else
+      render :edit
     end
   end
 
-  protected
+  def destroy
+    @softphone.destroy
 
-  def collection
-    @shared_softphones ||= end_of_association_chain.shared
+    redirect_to administration_shared_softphones_path
+  end
+
+  private
+  def set_softphone
+    @softphone = Softphone.find(params[:id])
+  end
+
+  def softphone_params
+    params.require(:softphone).permit(:name, :sip_username, :sip_password, :softphone_server_id)
   end
 end
