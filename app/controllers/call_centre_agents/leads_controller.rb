c17cfefc316a4c927828f9a88e88cdeb6bce2e38
Bartłomiej Danek <bartek.danek@gmail.com>
2014-10-20 09:59:22 +0200

    fix scope order for CallCentreAgents::LeadsController

--- app/controllers/call_centre_agents/leads_controller.rb
+++ app/controllers/call_centre_agents/leads_controller.rb
@@ -60,11 +60,15 @@ class CallCentreAgents::LeadsController < CallCentreAgents::CallCentreAgentContr
   protected
 
   def collection
-    if current_user
-      @categories = current_user.has_accessible_categories? ? LeadCategory.with_leads.within_accessible(current_user).without_locked_and_not_published : current_user.has_role?(:supplier) ? LeadCategory.with_leads.without_locked_and_not_published.with_supplier_unique(current_user) : LeadCategory.with_leads.without_locked_and_not_published.with_agent_unique(current_user)
-    else
-      @categories = LeadCategory.with_leads.without_locked_and_not_published.without_unique
-    end
+    @categories = if current_user
+                    current_user.has_accessible_categories? ?
+                      LeadCategory.with_leads.within_accessible(current_user).without_locked_and_not_published :
+                      (current_user.has_role?(:supplier) ?
+                       LeadCategory.with_supplier_unique(current_user).with_leads.without_locked_and_not_published :
+                       LeadCategory.with_agent_unique(current_user).with_leads.without_locked_and_not_published)
+                  else
+                    LeadCategory.without_unique.with_leads.without_locked_and_not_published
+                  end
 
     params[:search]||={}
 
