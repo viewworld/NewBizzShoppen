cec57b2d0c8e75773de1a6846eaa49eab42c4b0a
socjopata <socjopata@gmail.com>
2014-10-29 16:16:22 +0100

    Response properly with js #fl-610 comment

--- app/controllers/administration/invoicing/sellers_controller.rb
+++ app/controllers/administration/invoicing/sellers_controller.rb
@@ -1,5 +1,10 @@
 class Administration::Invoicing::SellersController < Administration::AdministrationController
   def update
     @seller = Seller.find(params[:seller_id])
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
