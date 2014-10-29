2dfa0bab071ea39c4b2b448f0818dd9a0be6ad30
socjopata <socjopata@gmail.com>
2014-10-29 13:15:06 +0100

    Fix multiple issues with the javascripts in the commited files. IF someone knows why the set_phone_code fuction defined in the view may be not defined for the phone_codes/index.js please let me know #fl-610 comment

--- app/controllers/phone_codes_controller.rb
+++ app/controllers/phone_codes_controller.rb
@@ -1,5 +1,9 @@
 class PhoneCodesController < ApplicationController
   def index
     @country = Country.find_by_id(params[:country_id])
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
--- app/views/phone_codes/index.js
+++ app/views/phone_codes/index.js
@@ -1,6 +1,10 @@
+function set_phone_code(field, code) {
+    $("[id$=" + field + "]").val('+' + code);
+}
+
 <% if @country %>
-  set_phone_code('lead_direct_phone_number', '<%= @country.phone_dialling_code %>');
-  set_phone_code('lead_phone_number', '<%= @country.phone_dialling_code %>');
-  set_phone_code('lead_company_phone_number', '<%= @country.phone_dialling_code %>');
+  set_phone_code('_direct_phone_number', '<%= @country.phone_dialling_code %>');
+  set_phone_code('_phone_number', '<%= @country.phone_dialling_code %>');
+  set_phone_code('_company_phone_number', '<%= @country.phone_dialling_code %>');
 <% end %>
 
