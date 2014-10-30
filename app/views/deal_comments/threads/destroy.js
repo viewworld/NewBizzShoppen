dd5a94d66793dfcdc1c54d736a1093d007b1dc57
socjopata <socjopata@gmail.com>
2014-10-30 13:03:22 +0100

    Applying feedback for ccd4ac #fl-610 comment

--- app/views/deal_comments/threads/destroy.js
+++ app/views/deal_comments/threads/destroy.js
@@ -1,7 +1,7 @@
 <% if @thread.nil? %>
   $('#thread_<%= @thread_id %>').remove();
 <% else %>
-  var content = '<%= escape_javascript(render(:partial => '/deal_comments/shared/thread', :locals => {:thread => @thread})) %>';
+  var content = '<%= escape_javascript(render('deal_comments/shared/thread', { thread: @thread })) %>';
   $('#thread_<%= @thread_id %>').replaceWith(content);
 <% end %>
 $('#truncated_thread_<%= @thread_id %>').remove();
