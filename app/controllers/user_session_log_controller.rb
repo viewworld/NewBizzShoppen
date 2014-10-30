86aa5904cfe12aca2d452e151ca4237065fc80cb
socjopata <socjopata@gmail.com>
2014-10-30 12:44:47 +0100

    Fix auto logging out popup #fl-610 comment

--- app/controllers/user_session_log_controller.rb
+++ app/controllers/user_session_log_controller.rb
@@ -1,5 +1,10 @@
 class UserSessionLogController < ApplicationController
   def create
     @user_session_log = UserSessionLog.active_regular_for_user(current_user).first
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
