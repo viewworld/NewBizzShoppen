2aa32ffeee78a3fdd0fb7eabb276e704585b6636
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-11-03 13:30:26 +0100

    add spec for admin articles controller

---
+++ spec/controllers/administration/articles_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Administration::ArticlesController do
+  before { create(:locale) }
+  include_context 'admin signed in'
+
+  let(:article) { create(:cms_main_page_article) }
+
+  describe '#new' do
+    let(:call_request) { get :new }
+    it_behaves_like 'an action rendering view'
+  end
+
+  describe '#create' do
+    let(:attributes) { attributes_for(:cms_main_page_article) }
+    let(:call_request) { post :create, article: attributes }
+
+    it_behaves_like 'an action creating object'
+  end
+
+  describe '#edit' do
+    let (:call_request) { get :edit, id: article.id }
+    it_behaves_like 'an action rendering view'
+  end
+
+  describe '#update' do
+    let(:attributes) { attributes_for(:cms_main_page_article) }
+    let(:call_request) { put :update, id: article.id, article: attributes }
+
+    it_behaves_like 'an action updating object'
+  end
+
+  describe '#destroy' do
+    let(:call_request) { delete :destroy, id: article.id }
+    it_behaves_like 'an action destroying object'
+  end
+end
