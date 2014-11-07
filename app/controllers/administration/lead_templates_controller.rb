d8c7ba9a4a7bea4b7bf7c612a63fd31e381fddf2
Mateusz Swiszcz <m.swiszcz@selleo.com>
2014-11-07 12:34:51 +0100

    refactor admin lead_templates

--- app/controllers/administration/lead_templates_controller.rb
+++ app/controllers/administration/lead_templates_controller.rb
@@ -1,4 +1,5 @@
 class Administration::LeadTemplatesController < Administration::AdministrationController
+  include Controllers::CommonActions
   set_tab 'browse_leads'
 
   before_filter :set_lead_template, only: [:edit, :update, :destroy]
@@ -9,36 +10,39 @@ class Administration::LeadTemplatesController < Administration::AdministrationCo
   end
 
   def create
-    @lead_template = LeadTemplate.new(params[:lead_template])
+    @lead_template = LeadTemplate.new(lead_template_params)
     @lead_template.creator = current_user
     @lead_template.current_user = current_user
 
-    if @lead_template.save
-      redirect_to edit_administration_category_path(@lead_template.category)
-    else
-      render :new
-    end
+    common_save(@lead_template, success: {
+      redirect: edit_administration_category_path(@lead_template.category)
+    })
   end
 
   def update
+    @lead_template.assign_attributes(lead_template_params)
     @lead_template.current_user = current_user
 
-    if @lead_template.update_attributes(params[:lead_template])
-      redirect_to edit_administration_category_path(@lead_template.category)
-    else
-      render :edit
-    end
+    common_save(@lead_template, success: {
+      redirect: edit_administration_category_path(@lead_template.category)
+    })
   end
 
   def destroy
     @lead_template.current_user = current_user
 
-    notice_key = @lead_template.destroy ? :deletion_successful : :deletion_failure
-    redirect_to :back, notice: t(notice_key, scope: 'administration.lead_templates.destroy.flash')
+    common_destroy(@lead_template)
   end
 
   private
+
   def set_lead_template
     @lead_template = LeadTemplate.find(params[:id]).decorate
+  end
+
+  def lead_template_params
+    params.require(:lead_template).permit(:name, :category_id, :is_global,
+      :is_mandatory, :is_active, lead_template_fields_attributes:
+      [:name, :field_type, :is_hidden, :is_mandatory, :_destroy, :id])
   end
 end
